a9769c2d2655dba87439f729d49b0c696ec2ad83
Will <pabiterowilliam@yahoo.com>
2022-05-18 02:01:18 -0400

    Added functionality

--- Javascript/main.js
+++ Javascript/main.js
@@ -9,17 +9,25 @@ function Book(title, author, pages, read) {
     }
 }
 
-let myLibrary = [];
+let myLibrary = []
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
 function displayMyLibrary() {
-    for (let book in myLibrary) {
-        let bookDiv = document.getElementsByClassName('book')
-        bookDiv.innerHtml = book.title;
+    let index = 0
+    for (let book of myLibrary) {
+        let bookDiv = document.getElementsByClassName('book')[index++]
+        bookDiv.innerHTML = book.info()
     }
 }
-
 const potter = new Book('Harry Potter', 'J.K. Rowling', 500, false)
+const frost = new Book('Jack Frost', 'Santa Claus', 100, false)
+const think = new Book('Think and Grow Rich', 'Something something', 100, true)
+
+addBookToLibrary(potter)
+addBookToLibrary(frost)
+addBookToLibrary(think)
+
+displayMyLibrary()
