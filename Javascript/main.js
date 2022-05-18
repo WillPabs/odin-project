6a71626a1c823c5788f3673a163fc5bd68056347
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-18 14:19:46 -0400

    Added new form book and respective event listeners. Added button on each book's display

--- Javascript/main.js
+++ Javascript/main.js
@@ -9,8 +9,6 @@ function Book(title, author, pages, read) {
     }
 }
 
-let myLibrary = []
-
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -20,9 +18,42 @@ function displayMyLibrary() {
     let cardDivElements = document.querySelectorAll('.card')
     myLibrary.forEach(book => {
         cardDivElements[index].children.item(0).innerHTML = book.title
-        cardDivElements[index++].children.item(1).innerHTML = book.info()
+        cardDivElements[index].children.item(1).innerHTML = book.info()
+        let removeBookButton = document.createElement('button')
+        removeBookButton.innerHTML = "Remove from Library"
+        cardDivElements[index].children.item(1).append(removeBookButton)
+        index++
     })
 }
+
+
+function addNewBook(book) {
+    const newBook = new Book(book.title, book.author, book.pages, book.read)
+    addBookToLibrary(newBook)
+    alert('Added new book!')
+}
+
+const bookForm = document.querySelector('#new-book-form')
+bookForm.addEventListener('submit', () => {
+    console.log(bookForm.classList)
+    alert('submit clicked')
+})
+
+
+const newBookButton = document.querySelector('#new-book-button')
+newBookButton.addEventListener('click', () => {
+    const newBookForm = document.querySelector('.new-book-form')
+
+    if (newBookForm.style.display === 'none') {
+        newBookForm.style.display = 'block'
+    } else {
+        newBookForm.style.display = 'none'
+    }
+})
+
+
+
+let myLibrary = []
 const potter = new Book('Harry Potter', 'J.K. Rowling', 500, false)
 const frost = new Book('Jack Frost', 'Santa Claus', 100, false)
 const think = new Book('Think and Grow Rich', 'Something something', 100, true)
