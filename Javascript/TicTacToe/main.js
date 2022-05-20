f342b9cc8d83ac0cd37b4ae2c3d7de150223093a
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-20 16:10:12 -0400

    Updated displayGameBoard function to dynamically render gameboard array to browser

--- Javascript/TicTacToe/main.js
+++ Javascript/TicTacToe/main.js
@@ -1,8 +1,17 @@
 // Gameboard object : module pattern
     // gameboard is an array
+    // reset() : sets all spots in gameboard back to empty
+    // displayGameboard() : renders the gameboard to the frontend
+    // updateGameboard(position, player) : updates the gameboard with the
+        // selected position with the player's marker
 const Gameboard = (function() {
     let gameboard;
     console.log("Starting game");
+    let dummyGameboard = [
+        ['X','O','O'],
+        ['O','X','O'],
+        ['X','O','O'],
+    ];
 
     const reset = () => {
         gameboard = Array.from(Array(3), () => new Array(3));
@@ -10,19 +19,31 @@ const Gameboard = (function() {
     };
 
     const displayGameboard = () => {
-        
-    }
+        console.log('displaying board')
+        let board = document.createElement('table');
+        dummyGameboard.forEach(row => { // change to use gameboard
+            let rowEl = document.createElement('div');
+            row.forEach(marker => {
+                let cell = document.createElement('span');
+                cell.innerHTML = marker;
+                rowEl.appendChild(cell);
+            });
+            board.appendChild(rowEl);
+        });
+        document.querySelector('#game-container').appendChild(board);
+    };
 
     return {
-        reset
+        reset,
+        displayGameboard
     };
 })();
 
-Gameboard.reset();
+Gameboard.displayGameboard();
 
 
 // Player object : factoryFunction pattern
-const playerFactory = (name) => {
+const Player = (name) => {
     let wins = 0;
     const getName = () => name;
 
@@ -31,8 +52,15 @@ const playerFactory = (name) => {
     }
 }
 
-const will = playerFactory('will')
+const will = Player('will')
 console.log(will.getName());
 
 
 // Game Flow Control object : module pattern
+    // hasWinner() : checks if a player's markers match horizontally
+        // vertically, or diagonally
+    // boardComplete() : checks if all spots on the board have been
+        // used. Will log no winner, if last spot didn't make a winner
+const GameFlowControl = () => {
+
+}
