2fb4c020c8d073d2bd8648406fde475aa42138c4
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-23 12:05:57 -0400

    Set up basic functionality to listen for click event and trigger setPosition() on gameboard

--- Javascript/TicTacToe/main.js
+++ Javascript/TicTacToe/main.js
@@ -13,18 +13,33 @@ const Gameboard = (function() {
         ['X','O','O'],
     ];
 
+    const getBoard = () => {
+        console.log(dummyGameboard);
+    };
+
     const reset = () => {
         gameboard = Array.from(Array(3), () => new Array(3));
         console.log('Reset game board');
     };
 
+    const setPosition = (position, marker) => {
+        console.log('Setting position on board');
+        if (position >= 0 && position < 3 ) {
+            dummyGameboard[0][position] = marker;
+        } else {
+            let rowNum = Math.floor(position / 3);
+            let newPosition = position % 3;
+            dummyGameboard[rowNum][newPosition] = marker;
+        };
+    };
+
     const displayGameboard = () => {
         console.log('displaying board')
         let board = document.querySelector('#game-container');
-        let index = 1;
+        let index = 0;
         dummyGameboard.forEach(row => { // change to use gameboard
             row.forEach(marker => {
-                let cell = document.querySelector(`#cell${index}`);
+                let cell = document.querySelector(`#cell-${index}`);
                 cell.innerHTML = marker;
                 board.appendChild(cell);
                 index++;
@@ -34,11 +49,13 @@ const Gameboard = (function() {
 
     return {
         reset,
-        displayGameboard
+        displayGameboard,
+        getBoard,
+        setPosition
     };
 })();
 
-Gameboard.displayGameboard();
+
 
 
 // Player object : factoryFunction pattern
@@ -49,27 +66,27 @@ const Player = (name) => {
     const getMarker = () => marker;
     const getName = () => name;
 
-    const selectCell = (position) => {
-        const selected = document.querySelector(`#cell${position}`)
-        // check if position on board is filled
-        if (selected.innerHTML === null)
-            // return alert position has been filled choose another
-            alert('Cell has already been marked! Choose another cell.')
-        // grab the element of the position
-        selected.addEventListener('click', () => {
-            selected.innerHTML = marker;
-        })
-        // set a click event for that position
-            // fill the element with the player's marker
-    } 
+    // const selectCell = (position) => {
+    //     const selected = document.querySelector(`#cell${position}`)
+    //     // check if position on board is filled
+    //     if (selected.innerHTML === null)
+    //         // return alert position has been filled choose another
+    //         alert('Cell has already been marked! Choose another cell.')
+    //     // grab the element of the position
+    //     selected.addEventListener('click', () => {
+    //         selected.innerHTML = getMarker();
+    //     })
+    //     // set a click event for that position
+    //         // fill the element with the player's marker
+    // } 
 
     return {
+        getMarker,
         getName
     }
 }
 
-const will = Player('will')
-console.log(will.getName());
+
 
 
 // Game Flow Control object : module pattern
@@ -77,18 +94,21 @@ console.log(will.getName());
         // vertically, or diagonally
     // boardComplete() : checks if all spots on the board have been
         // used. Will log no winner, if last spot didn't make a winner
-const GameFlowControl = (player1) => {
+const GameFlowControl = function() {
+    Gameboard.displayGameboard();
+    const will = Player('will');
+    
     
     document.querySelectorAll('.cell').forEach(cell => {
         cell.addEventListener('click', e => {
             let target = e.target;
-            console.log(target.id)
-            if (target.nodeName === 'cell') {
-                console.log(target);
-                target.innerHTML = player1.getMarker();
-            };
+            if (target.className === 'cell') {
+                console.log(target.id)
+                let position = target.id.split('-')[1];
+                Gameboard.setPosition(position, will.getMarker());
+                Gameboard.displayGameboard();
+            }
         });
     });
-};
+}();
 
-const gameflow = GameFlowControl(will)
