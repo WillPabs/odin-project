3c03b57bce3ac5b005b41d0c81d82f27efdd6d39
Will <pabiterowilliam@yahoo.com>
2022-05-31 01:38:16 -0400

    Set up operator functions and added calculator buttons to browser

---
+++ Javascript/Calculator/README.md
@@ -0,0 +1,12 @@
+# Calculator
+
+Operators
+- add
+- subtract
+- multiply
+- divide
+
+operate function that accepts 3 parameters and uses the operator on the 2 numbers
+- operator object
+- a number
+- a number
---
+++ Javascript/Calculator/index.js
@@ -0,0 +1,58 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
+
+const operate = (operator, num1, num2) => {
+    return operator(num1, num2);
+};
+
+function calculatorElement() {
+    let container = document.createElement('div');
+    container.className = 'container';
+
+    let buttonsContainer = document.createElement('div');
+    buttonsContainer.classList = 'buttonsContainer';
+
+    for (let i = 1; i < 10; i++) {
+        let button = document.createElement('button');
+        button.className = 'button';
+        button.id = `button-${i}`;
+        button.innerText = i;
+        buttonsContainer.appendChild(button);
+    };
+
+    let equalsElement = document.createElement('button');
+    equalsElement.className = 'equalsSign';
+    equalsElement.id = 'equals';
+    equalsElement.innerText = '=';
+
+    let clear = document.createElement('button');
+    clear.className = clear;
+    clear.id = 'clear';
+    clear.innerText = 'CLEAR';
+
+    container.appendChild(buttonsContainer);
+    container.appendChild(equalsElement);
+    container.appendChild(clear);
+
+    return container;
+}
+
+
+
+const calculator = calculatorElement();
+console.log(calculator);
+document.body.appendChild(calculator);
+
