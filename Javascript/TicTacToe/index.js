104c2c81a9328d7aef792b165f40d8d6a7daa8b7
Will <pabiterowilliam@yahoo.com>
2022-06-15 02:05:59 -0400

    Refactor

---
+++ Javascript/TicTacToe/index.js
@@ -0,0 +1,183 @@
+const Gameboard = function() {
+    let gameboard = new Array(9).fill('');
+    let dummyGameboard = [
+        ['X','O','O'],
+        ['O','X','O'],
+        ['X','O','O'],
+    ];
+
+    const winningCombos = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [3,4,5],
+        [6,7,8]
+    ];
+
+    let previousWinners = [];
+
+    const getWinners = () => {
+        return previousWinners;
+    }
+
+    const renderGameboard = () => {
+        let board = document.querySelector("#game-container");
+        
+        gameboard.forEach((cell, i) => {
+            let c = document.querySelector(`#cell-${i}`);
+            c.textContent = cell;
+            board.appendChild(c);
+        });
+    };
+
+    const setPosition = (position, marker) => {
+        gameboard[position] = marker;
+    }
+
+    const isTie = () => {
+        return gameboard.every(cell => cell === 'X' || cell === 'O');
+    };
+
+    const hasWinner = () => {
+        const marker1 = 'X';
+        const marker2 = 'O' ;
+        for (let combo = 0; combo < winningCombos.length; combo++) {
+            let m1MatchCount = 0;
+            let m2MatchCount = 0;
+            for (let position = 0; position < winningCombos[combo].length; position++) {
+                let gBoardPos = gameboard[winningCombos[combo][position]]; 
+                if (gBoardPos === marker1) m1MatchCount++;
+                if (gBoardPos === marker2) m2MatchCount++;
+            }
+            if (m1MatchCount === 3) {
+                let winner = {marker: marker1, combo : winningCombos[combo]};
+                previousWinners.push(winner);
+                return true;
+            };
+            if (m2MatchCount === 3) {
+                let winner = {marker: marker1, combo : winningCombos[combo]};
+                previousWinners.push(winner);
+                return true;
+            };
+        }
+        return false;
+    }
+
+    const isGameOver = () => {
+        if (hasWinner() || isTie()) return true;
+        return false;
+    }
+
+    const reset = () => {
+        gameboard = new Array(9).fill('');
+    }
+
+    return {
+        getWinners,
+        renderGameboard,
+        setPosition,
+        isGameOver,
+        reset
+    }
+}();
+
+const PlayerFactory = (name, marker) => {
+
+    const selectCell = (element, gameboard) => {
+        let selection = element.target; 
+        let isSelectionSet = false;
+        if (selection.textContent === 'X' || selection.textContent === 'O') {
+            alert('Cell has already been marked! Choose another cell.');
+            return isSelectionSet;
+        } else {
+            if (selection.className === 'cell') {
+                let position = selection.id.split('-')[1];
+                gameboard.setPosition(position, marker);
+                isSelectionSet = true;
+                return isSelectionSet;
+            };
+        };
+    };
+
+    return {
+        name,
+        marker,
+        selectCell
+    };
+};
+
+const GameFlowControl = (player1, player2) => {
+    let turnTracker = [];
+    
+    const setDetails = () => {
+        let p1Details = document.querySelector('#player1-details').children;
+        let p2Details = document.querySelector('#player2-details').children;
+        p1Details[0].textContent = player1.name;
+        p1Details[1].textContent = player1.marker;
+        p2Details[0].textContent = player2.name;
+        p2Details[1].textContent = player2.marker;
+    }
+
+    const setTurn = () => {
+        if (turnTracker.length === 0) {
+            turnTracker.push(player1);
+            return player1;
+        };
+
+        // player2's turn
+        if (turnTracker[turnTracker.length - 1] === player1) {
+            turnTracker.push(player2);
+            return player2;
+        };
+
+        // player1's turn
+        if (turnTracker[turnTracker.length - 1] === player2) {
+            turnTracker.push(player1);
+            return player1;
+        };
+    };
+
+    return {
+        setDetails,
+        setTurn
+    };
+    
+};
+
+let restart = document.querySelector('#restartButton');
+restart.addEventListener('click', () => {
+    Gameboard.reset();
+    Gameboard.renderGameboard();
+});
+
+/* SETUP */
+let name1 = prompt('Enter Player 1 name');
+const player1 = PlayerFactory(name1, 'X');
+let name2 = prompt('Enter Player 2 name');
+const player2 = PlayerFactory(name2, 'O');
+let game = GameFlowControl(player1, player2);
+game.setDetails();
+
+/* GAME START */
+let currentPlayer = game.setTurn(); // assignment of who takes 1st turn
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', (e) => {
+        if (currentPlayer.selectCell(e, Gameboard)) {
+            Gameboard.renderGameboard();
+            if(Gameboard.isGameOver()) {
+                let winningText = document.querySelector('#winning-message-text');
+                let winningInfo = Gameboard.getWinners().pop();
+                winningText.textContent = `${currentPlayer.name} is the winner!\n ${currentPlayer.name} won with ${winningInfo.combo}`;
+                setTimeout(() => {
+                    Gameboard.reset();
+                    alert("GAME OVER");
+                    Gameboard.renderGameboard();
+                }, 1000);
+            };
+            currentPlayer = game.setTurn();
+        };
+    });
+});
--- Javascript/TicTacToe/readme.md
+++ Javascript/TicTacToe/readme.md
@@ -1,38 +1,49 @@
-TicTacToe
-
+# TicTacToe
 USE CASE Description
 
-1. A player is prompted to enter their name and pick a marker. The other player gets assigned the other marker. One of the players is chosen at random to be the first player. Player one selects a cell on the gameboard. The selected cell is marked with that player's marker. Then it is player two's turn to select a cell on the gameboard. That cell is then marked with that respective player's marker. This repeats until there is a winner, signified by a player's marker consecutively appearing diagonally, vertically, or horizontally, or until the board is full. If the board is full and there is no winner, a prompt will appear asking to play again.
-
+A player is prompted to enter their name and pick a marker. The other player gets assigned the other marker. One of the players is chosen at random to be the first player. Player one selects a cell on the gameboard. The selected cell is marked with that player's marker. Then it is player two's turn to select a cell on the gameboard. That cell is then marked with that respective player's marker. This repeats until there is a winner, signified by a player's marker consecutively appearing diagonally, vertically, or horizontally, or until the board is full. If the board is full and there is no winner, a prompt will appear asking to play again.
 Triggers
 
-1. A player enters their name and marker
-
+A player enters their name and marker
 Actors
-    
-1. Player
-2. Gameboard
-3. GameFlowControl
 
+Player
+Gameboard
+GameFlowControl
 Preconditions
 
-1. 2 players are available
-2. A board is available  
-
+2 players are available
+A board is available
 Goals
 
-1. A player wins by consecutively matching their respective marker diagonally, vertically, or horizontally.
-
+A player wins by consecutively matching their respective marker diagonally, vertically, or horizontally.
 Steps of Execution
 
-1. Player one enters their name and preferred marker.
-2. Player two enters their name and gets the other marker.
-3. Player one clicks a cell from the board.
-4. If the cell is empty, the cell is filled with player one's marker
-5. If the cell is not empty, player one will be prompted to pick another cell
-6. The game will check if player one is a winner 
-7. Player two then gets to click a cell
-8. If the same condition above for player one is satisfied, the cell will be filled with player two's marker
-9. The game will check if player two is a winner
-10. The game will check if the board is full
-11. Repeat from step 5
+Player one enters their name and preferred marker.
+Player two enters their name and gets the other marker.
+Player one clicks a cell from the board.
+If the cell is empty, the cell is filled with player one's marker
+If the cell is not empty, player one will be prompted to pick another cell
+The game will check if player one is a winner
+Player two then gets to click a cell
+If the same condition above for player one is satisfied, the cell will be filled with player two's marker
+The game will check if player two is a winner
+The game will check if the board is full
+Repeat from step 5
+
+## Objects
+
+#### Players
+- name : String
+- marker : String
+- selectCell()
+
+#### Gameboard
+- gameboard : array
+- winningcombos : array
+- isGameOver()
+- setPosition()
+- reset()
+
+#### GameFlowControl
+- takeTurn()
