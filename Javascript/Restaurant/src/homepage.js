3aa8e2d277c295340f859ed9df326f09a60a1de0
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-15 16:38:12 -0400

    Added files, refactored and restructured

--- Javascript/Restaurant/src/homepage.js
+++ Javascript/Restaurant/src/homepage.js
@@ -1,19 +1,28 @@
-import './styles/style.css';
+import './styles/homepage.css';
 
 const bgImg = document.createElement('img');
 bgImg.src = 'https://images.unsplash.com/photo-1518188770546-efd25d4ca263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80';
+bgImg.style.height = '100%';
+bgImg.style.width = '100%';
+bgImg.style.objectFit = 'fill';
 
 const headline = document.createElement('div');
 headline.textContent = 'WELCOME TO MY RESTAURANT';
+headline.classList.add('headline');
 
 const copy = document.createElement('div');
 copy.textContent = 'We are a 5 Michelin star restuarant. Enjoy our finest cuisine in all of North America.';
+copy.classList.add('copy');
+
+const homeContainer = document.createElement('div');
+homeContainer.id = 'home-container';
+homeContainer.appendChild(headline);
+homeContainer.appendChild(copy);
 
 const addHomepage = () => {
     const content = document.querySelector('#content');
     content.appendChild(bgImg);
-    content.appendChild(headline);
-    content.appendChild(copy);
+    content.appendChild(homeContainer);
 }
 
 export { addHomepage };
--- Javascript/Restaurant/src/index.js
+++ Javascript/Restaurant/src/index.js
@@ -1,4 +1,5 @@
 import { addHomepage } from "./homepage";
+import './styles/style.css';
 
 console.log('Welcome to restaurant');
 addHomepage();
