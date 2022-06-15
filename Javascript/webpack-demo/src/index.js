147d2e874e078ecbbfb53ebbe96840d7f1c73017
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-15 13:29:03 -0400

    added files

--- Javascript/webpack-demo/src/index.js
+++ Javascript/webpack-demo/src/index.js
@@ -1,11 +1,14 @@
-import _ from 'lodash';
+import myName from './myName';
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(component());
+
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, are you ${time}?`);
