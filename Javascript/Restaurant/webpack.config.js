722da253d38d3a6418c3e82aeb6d539c41f0746c
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-16 13:44:25 -0400

    Restructure

--- Javascript/Restaurant/webpack.config.js
+++ Javascript/Restaurant/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/templates/index.html'
     }),
   ],
   module: {
