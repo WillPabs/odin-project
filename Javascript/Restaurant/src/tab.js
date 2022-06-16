3f9f31da8b9a441e4af0d6520a189aa644609420
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-16 16:29:39 -0400

    Added click event listener for respective tab

--- Javascript/Restaurant/src/tab.js
+++ Javascript/Restaurant/src/tab.js
@@ -1,18 +1,26 @@
 import './styles/tab.css';
+import { addMenu } from './menu';
+import { addHomepage } from './homepage';
+import { addContact } from './contact';
 
-const createTab = (text) => {
+const createTab = (text, callback) => {
     const container = document.createElement('div');
     container.classList.add('tab-container');
     const tab = document.createElement('button');
+    tab.addEventListener('click', () => {
+        let content = document.querySelector('.container').parentElement;
+        document.querySelector('.container').remove();
+        content.appendChild(callback());
+    });
     tab.classList.add('tab');
     tab.textContent = text;
     container.appendChild(tab);
     return container;
 }
 
-const home = createTab('Home');
-const menu = createTab('Menu');
-const contact = createTab('Contact');
+const home = createTab('Home', addHomepage);
+const menu = createTab('Menu', addMenu);
+const contact = createTab('Contact', addContact);
 
 const container = document.createElement('div');
 container.classList.add('tabs');
@@ -22,6 +30,5 @@ container.appendChild(menu);
 container.appendChild(contact);
 
 export const addTab = () => {
-    const content = document.querySelector('#content');
-    content.appendChild(container);
+    return container;
 }
