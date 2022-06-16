c324ee17f8b5381a837438c81084def9c5dab565
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-16 13:31:48 -0400

    Added htmlwebpackplugin

--- Javascript/Restaurant/webpack.config.js
+++ Javascript/Restaurant/webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant',
+      templateContent: `
+        <html>
+          <body>
+            <div id='content'></div>
+          </body>
+        </html
+      `
+    }),
+  ],
   module: {
     rules: [
       {
