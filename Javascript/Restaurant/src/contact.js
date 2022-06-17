0cc454b0107b903a49919602c35dccbdc2d49caa
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-17 13:31:07 -0400

    Added data

--- Javascript/Restaurant/src/contact.js
+++ Javascript/Restaurant/src/contact.js
@@ -1,28 +1,21 @@
 export const addContact = () => {
-    const container = document.createElement('div');
-    container.id = 'contact-container';
-    container.classList.add('container');
-
-    const headline = document.createElement('div');
-    headline.classList.add('headline');
-    headline.textContent = 'Contact Us';
-
+    
     const createContact = (position, name, number, email, profilePicture) => {
         const contactContainer = document.createElement('div');
         contactContainer.classList.add('contact');
         
         const positionDiv = document.createElement('div');
         positionDiv.textContent = position;
-
+        
         const nameDiv = document.createElement('div');
         nameDiv.textContent = name;
 
         const numberDiv = document.createElement('div');
         numberDiv.textContent = number;
-
+        
         const emailDiv = document.createElement('div');
         emailDiv.textContent = email;
-
+        
         const profilePictureDiv = document.createElement('img');
         profilePictureDiv.src = profilePicture;
 
@@ -35,8 +28,19 @@ export const addContact = () => {
         return contactContainer;
     };
 
+    const container = document.createElement('div');
+    container.id = 'contact-container';
+    container.classList.add('container');
+    
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = 'Contact Us';
+    container.appendChild(headline);
+
     const will = createContact('CEO', 'William Pabitero', '111-111-1111', 'goated@dev.com', undefined);
+    const elon = createContact('Chief Technoking', 'Elon Musk', '123-999-0000', 'elon@musk.com', undefined);
     container.appendChild(will);
+    container.appendChild(elon);
 
     return container;
 }
