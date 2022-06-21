9e2273509603321725fad6f50abd4ce652da03cd
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-21 15:50:03 -0400

    Added corresponding images

--- Javascript/Restaurant/src/menu.js
+++ Javascript/Restaurant/src/menu.js
@@ -1,4 +1,12 @@
 import './styles/style.css';
+import steakImg from './images/menu/steak.avif';
+import salmonImg from './images/menu/salmon.avif';
+import chickenImg from './images/menu/chicken.avif';
+import seltzerImg from './images/menu/seltzer.avif';
+import beerImg from './images/menu/beer.avif';
+import tequilaImg from './images/menu/tequila.avif';
+import friesImg from './images/menu/fries.avif';
+import saladImg from './images/menu/salad.avif';
 
 const createMenuGroup = (text) =>  {
     const outer = document.createElement('div');
@@ -24,21 +32,21 @@ const createMenuItem = (name, price, description, image) => {
     itemName.classList.add('item-name');
     itemName.textContent = name;
 
-    const itemPrice = document.createElement('div');
-    itemPrice.classList.add('item-price');
-    itemPrice.textContent = price;
-    
     const itemDesc = document.createElement('div');
     itemDesc.classList.add('item-desc');
     itemDesc.textContent = description;
+
+    const itemPrice = document.createElement('div');
+    itemPrice.classList.add('item-price');
+    itemPrice.textContent = price;
     
     const itemImage = document.createElement('img');
     itemImage.classList.add('item-image');
     itemImage.src = image;
     
     item.appendChild(itemName);
-    item.appendChild(itemPrice);
     item.appendChild(itemDesc);
+    item.appendChild(itemPrice);
     item.appendChild(itemImage);
     outer.appendChild(item);
     return outer;
@@ -60,26 +68,26 @@ menuContainer.appendChild(headline);
 const drinkImg = 'https://images.unsplash.com/photo-1587888637140-849b25d80ef9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80';
 
 const mainDish = createMenuGroup('Main Dishes');
-const steak = createMenuItem('Tomahawk Steak', '$1000', 'Rare steak from the fields of Japan', drinkImg);
-const salmon = createMenuItem('Fresh Atlantic Salmon', '$49.99', 'Fresh caught wild salmon of the Atlantic', drinkImg);
-const chicken = createMenuItem('Grilled Peruvian Chicken', '$29.99', 'Organic grass fed chicken', drinkImg);
+const steak = createMenuItem('Tomahawk Steak', '$1000', 'Rare steak from the fields of Japan', steakImg);
+const salmon = createMenuItem('Fresh Atlantic Salmon', '$49.99', 'Fresh caught wild salmon of the Atlantic', salmonImg);
+const chicken = createMenuItem('Grilled Peruvian Chicken', '$29.99', 'Organic grass fed chicken', chickenImg);
 menuContainer.appendChild(mainDish);
 menuContainer.appendChild(steak);
 menuContainer.appendChild(salmon);
 menuContainer.appendChild(chicken);
 
 const bevs = createMenuGroup('Beverages');
-const seltzer = createMenuItem('Happy Dad', '$9.99', 'Hard Seltzer from Nelk Boys', drinkImg);
-const beer = createMenuItem('Stella Artois', '$7.99', 'Fresh beer', drinkImg);
-const tequila = createMenuItem('Casamigos', '$39.99', 'Tequila baby!!!', drinkImg);
+const seltzer = createMenuItem('Happy Dad', '$9.99', 'Hard Seltzer from Nelk Boys', seltzerImg);
+const beer = createMenuItem('Stella Artois', '$7.99', 'Fresh beer', beerImg);
+const tequila = createMenuItem('Casamigos', '$39.99', 'Tequila baby!!!', tequilaImg);
 menuContainer.appendChild(bevs);
 menuContainer.appendChild(seltzer);
 menuContainer.appendChild(beer);
 menuContainer.appendChild(tequila);
 
 const sides = createMenuGroup('Sides');
-const fries = createMenuItem('French Fries', '$4.99', 'Fresh cut potatoes', drinkImg);
-const salad = createMenuItem('Kale Salad', '$6.99', 'Fresh vegetable salad', drinkImg);
+const fries = createMenuItem('French Fries', '$4.99', 'Fresh cut potatoes', friesImg);
+const salad = createMenuItem('Kale Salad', '$6.99', 'Fresh vegetable salad', saladImg);
 menuContainer.appendChild(sides);
 menuContainer.appendChild(fries);
 menuContainer.appendChild(salad);
