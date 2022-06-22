c4ef7d200172d71647700321e92b01c160e524b3
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-22 15:32:47 -0400

    Configured webpack to bundle images into single output folder

--- Javascript/Restaurant/webpack.config.js
+++ Javascript/Restaurant/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
     clean: true
   },
   devtool: 'inline-source-map',
