0feda7ecd37aa4b85244c8a9cfa4accaa37656c4
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-27 16:21:53 -0400

    Added logic to create project

--- Javascript/todo-list/src/project.js
+++ Javascript/todo-list/src/project.js
@@ -1,3 +1,35 @@
-const Project = () => {
+export const Project = (title) => {
+    const taskList = [];
+    const finishedTasks = [];
 
+    const addTask = (task) => {
+        taskList.push(task);
+    };
+
+    const removeTask = (task) => {
+        const index = taskList.findIndex(task);
+        taskList.slice(index, 1);
+    };
+
+    const finishTask = (task) => {
+        if (task in taskList) {
+            finishedTasks.push(task);
+        };
+    };
+
+    const getTaskList = () => {
+        return taskList;
+    };
+
+    const getFinishedTasks = () => {
+        return finishedTasks;
+    };
+
+    return {
+        addTask,
+        removeTask,
+        finishTask,
+        getTaskList,
+        getFinishedTasks
+    }
 };
