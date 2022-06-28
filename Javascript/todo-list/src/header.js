969094bfd297a29cb66e7149e19a4862a4d3d001
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-28 11:29:01 -0400

    Created logic for header component

---
+++ Javascript/todo-list/src/header.js
@@ -0,0 +1,24 @@
+export const Header = (profilePic, name) => {
+    const container = document.createElement('div');
+    container.classList.add('header');
+
+    const logo = document.createElement('img');
+    logo.classList.add('logo');
+    logo.src = '';
+
+    const profilePicDiv = document.createElement('img');
+    profilePicDiv.classList.add('profile-pic');
+    profilePicDiv.src = profilePic;
+    profilePicDiv.style.width = '50px';
+
+    const nameDiv = document.createElement('div');
+    nameDiv.classList.add('name');
+    nameDiv.textContent = name;
+
+
+    container.appendChild(logo);
+    container.appendChild(profilePicDiv);
+    container.appendChild(nameDiv);
+
+    return container;
+}
