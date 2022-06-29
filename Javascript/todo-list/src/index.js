416c35d507476a8f0405d52e2a590509c7f6a6a8
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-29 14:26:58 -0400

    Added data

--- Javascript/todo-list/src/index.js
+++ Javascript/todo-list/src/index.js
@@ -1,12 +1,16 @@
-import { Task, TaskNode } from './task';
+import { Task } from './task';
 import { Project } from './project';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { TaskNode } from './components/Task';
 import { ProjectBar } from './components/ProjectBar';
 import { CreateTask } from './components/CreateTask';
 import { CreateProject } from './components/CreateProject';
 import { Tasks } from './components/Tasks';
+import { Projects } from './components/Projects';
+import { User } from './user';
 import pfp1 from './images/DeadGod_5011.png';
+import './styles/styles.css';
 
 const header = Header(pfp1, 'William Pabitero');
 document.body.appendChild(header);
@@ -26,6 +30,17 @@ project.addTask(task);
 project.addTask(task1);
 project.addTask(task2);
 
+const user1 = User('will');
+user1.addProject(project);
+
+const projectsView = Projects(user1);
+document.body.appendChild(projectsView);
+
+setTimeout(() => {
+    task.setTitle('Nothing')
+    console.log(task)
+}, 1000);
+
 const projectBar = ProjectBar(project);
 document.body.appendChild(projectBar);
 
@@ -34,3 +49,8 @@ document.body.appendChild(tasksView);
 
 const footer = Footer();
 document.body.appendChild(footer);
+
+const changeTitle = document.createElement('button');
+changeTitle.addEventListener('click', () => {
+    // document.querySelector('')
+});
