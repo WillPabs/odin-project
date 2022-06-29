755a3b5d0d2f263e1651ab8384b3b3f9cf4d5523
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-29 12:07:59 -0400

    Refactor

--- Javascript/todo-list/src/components/Header.js
+++ Javascript/todo-list/src/components/Header.js
@@ -4,9 +4,22 @@ export const Header = (profilePic, name) => {
     const container = document.createElement('div');
     container.classList.add('header');
 
+    const left = document.createElement('div');
+    left.classList.add('header-left');
+
     const logo = document.createElement('img');
     logo.classList.add('logo');
     logo.src = logoIcon;
+
+    const logoName = document.createElement('div');
+    logoName.classList.add('logo-name');
+    logoName.textContent = 'FulFill';
+    left.appendChild(logo);
+    left.appendChild(logoName);
+    
+
+    const right = document.createElement('div');
+    right.classList.add('header-right');
 
     const profilePicDiv = document.createElement('img');
     profilePicDiv.classList.add('profile-pic');
@@ -16,11 +29,10 @@ export const Header = (profilePic, name) => {
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('name');
     nameDiv.textContent = name;
+    right.appendChild(profilePicDiv);
+    right.appendChild(nameDiv);
 
-
-    container.appendChild(logo);
-    container.appendChild(profilePicDiv);
-    container.appendChild(nameDiv);
-
+    container.appendChild(left);
+    container.appendChild(right);
     return container;
 }
