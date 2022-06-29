5a3058c817bce330998b84778a4ff0c304ed67fd
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-29 13:47:50 -0400

    Decoupled TaskNode function from task.js

---
+++ Javascript/todo-list/src/components/Task.js
@@ -0,0 +1,30 @@
+export const TaskNode = (task) => {
+    const taskNode = document.createElement('div');
+    taskNode.classList.add('task');
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('task-title');
+    titleDiv.textContent = task.title;
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('task-description');
+    descriptionDiv.textContent = task.description;
+
+    const dueDateDiv = document.createElement('div');
+    dueDateDiv.classList.add('task-due-date')
+    dueDateDiv.textContent = task.dueDate;
+
+    const priorityDiv = document.createElement('div');
+    priorityDiv.classList.add('task-priority');
+    priorityDiv.textContent = task.priority;
+
+    const notesDiv = document.createElement('div');
+    notesDiv.classList.add('task-notes');
+    notesDiv.textContent = task.notes;
+
+    taskNode.appendChild(titleDiv);
+    taskNode.appendChild(descriptionDiv);
+    taskNode.appendChild(dueDateDiv);
+    taskNode.appendChild(priorityDiv);
+    taskNode.appendChild(notesDiv);
+    return taskNode;
+};
