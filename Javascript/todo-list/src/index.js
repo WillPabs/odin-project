e83a82a255cd68728f87d6d562229d48185f6e04
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-30 14:24:34 -0400

    Added data

--- Javascript/todo-list/src/index.js
+++ Javascript/todo-list/src/index.js
@@ -9,19 +9,12 @@ import { CreateProject } from './components/CreateProject';
 import { Tasks } from './components/Tasks';
 import { Projects } from './components/Projects';
 import { User } from './user';
+import { Content } from './components/Content';
 import pfp1 from './images/DeadGod_5011.png';
 import './styles/styles.css';
 
-const header = Header(pfp1, 'William Pabitero');
-document.body.appendChild(header);
-
-const createProject = CreateProject();
-
-const createTask = CreateTask();
 
 const task = Task('Read', 'Finish Chapter 1', '6/28/22', 'high');
-const taskNode = TaskNode(task);
-
 const task1 = Task('Dentist Appointment', '4:30pm sharp at 14 Meadows Lane', '6/28/22', 'medium');
 const task2 = Task('Do dishes', 'Put everything in dishwasher except glasses', '6/28/22', 'low');
 
@@ -30,25 +23,36 @@ project.addTask(task);
 project.addTask(task1);
 project.addTask(task2);
 
-const user1 = User('will');
+const project1 = Project('Project2');
+project1.addTask(task);
+project1.addTask(task1);
+project1.addTask(task2);
+
+const user1 = User('William Pabitero');
+user1.addProfilePicture(pfp1);
 user1.addProject(project);
+user1.addProject(project1);
+
+const header = Header(user1);
+document.querySelector('header').appendChild(header);
+
+const main = document.querySelector('main');
+
+const projectBar = ProjectBar(user1.projects);
+main.appendChild(projectBar);
+
 
 const projectsView = Projects(user1);
-document.body.appendChild(projectsView);
-
-setTimeout(() => {
-    task.setTitle('Nothing')
-    console.log(task)
-}, 1000);
-
-const projectBar = ProjectBar(project);
-document.body.appendChild(projectBar);
+// main.appendChild(projectsView);
 
 const tasksView = Tasks(project);
-document.body.appendChild(tasksView);
+// main.appendChild(tasksView);
+
+const content = Content(tasksView);
+main.appendChild(content);
 
 const footer = Footer();
-document.body.appendChild(footer);
+document.querySelector('footer').appendChild(footer);
 
 const showCreate = (head, toInsert, callback) => {
     if (!toInsert) {
@@ -71,12 +75,17 @@ const toggleShow = (toInsert) => {
 const createProjectLink = document.querySelector('.create-project-link');
 const createTaskLink = document.querySelector('.create-task-link');
 
-createProjectLink.addEventListener('click', () => {
-    const head = document.querySelector('.projects-header')
-    const toInsert = document.querySelector('#create-project')
-    showCreate(head, toInsert, CreateProject);
-});
+// createProjectLink.addEventListener('click', () => {
+//     const head = document.querySelector('.projects-header')
+//     const toInsert = document.querySelector('#create-project')
+//     showCreate(head, toInsert, CreateProject);
+// });
 
+
+// TODO
+// fix bug that no longer displays create task component
+// when changing project on project bar and clicking
+// create task
 createTaskLink.addEventListener('click', () => {
     const head = document.querySelector('.project');
     const toInsert = document.querySelector('#create-task');
