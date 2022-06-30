e3db9a0ead2d8a2a56505ae4502d152b0499c95c
Will Pabs <pabiterowilliam@yahoo.com>
2022-06-30 14:21:01 -0400

    Added event listener to switch content container to display a project component

--- Javascript/todo-list/src/components/ProjectBar.js
+++ Javascript/todo-list/src/components/ProjectBar.js
@@ -1,3 +1,6 @@
+import { switchContent } from './Content';
+import { Tasks } from './Tasks';
+
 export const ProjectBar = (...projects) => {
     const projectBar = document.createElement('div');
     projectBar.id = 'projects-bar';
@@ -7,18 +10,28 @@ export const ProjectBar = (...projects) => {
     heading.textContent = 'Your Projects';
     projectBar.appendChild(heading);
 
-    createProjectLinks(projects, projectBar);
+    const links = createProjectLinks(...projects);
+    projectBar.appendChild(links);
     return projectBar;
 };
 
-const createProjectLinks = (projects, container) => {
+const createProjectLinks = (projects) => {
+    const container = document.createElement('div');
+    container.classList.add('project-links');
     projects.forEach(project => {
         const link = document.createElement('a');
         link.classList.add('project-link');
         link.href = '#';
         link.textContent = project.getTitle();
-
+        link.addEventListener('click', () => {
+            switchContent(Tasks(project));
+        })
         container.appendChild(link);
     });
     return container;
 };
+
+// TODO
+// add link at bottom of bar with text 'View All Projects'
+// when link is clicked, it should trigger the content component
+// to switch to Projects View Component
