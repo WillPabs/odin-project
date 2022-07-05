f0a2722ef29968517eba1f8d976254a7316955ab
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-05 12:38:47 -0400

    Added finished property and function to toggle

--- Javascript/todo-list/src/task.js
+++ Javascript/todo-list/src/task.js
@@ -5,12 +5,22 @@ export const Task = (title, description, dueDate, priority, notes) => {
     self.dueDate = dueDate;
     self.priority = priority;
     self.notes = notes;
+    self.finished = false;
     
     return Object.assign(self, taskFunctions);
 };
 
+const Priority = {
+    HIGH: 'High',
+    MEDIUM: 'Medium',
+    LOW: 'Low'
+}
+
 const taskFunctions = {
     setTitle(newTitle) {
         this.title = newTitle;
+    },
+    toggleFinished() {
+        this.finished === true ? this.finished = false : this.finished = true;
     }
 };
