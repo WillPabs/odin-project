705d2444437153b88e4e3087dd2e7a05e9657344
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-11 15:46:57 -0400

    Moved title, priority and duedate into a task header container

--- Javascript/todo-list/src/components/Task.js
+++ Javascript/todo-list/src/components/Task.js
@@ -4,25 +4,13 @@ export const TaskNode = (task) => {
     const taskNode = document.createElement('div');
     taskNode.classList.add('task');
 
+    const taskHeader = document.createElement('div');
+    taskHeader.classList.add('task-header');
+    
     const titleDiv = document.createElement('div');
     titleDiv.classList.add('task-title');
     titleDiv.textContent = task.title;
-
-    const descriptionDiv = document.createElement('div');
-    descriptionDiv.classList.add('task-description');
-    const label = document.createElement('label');
-    label.classList.add('description-label');
-    label.textContent = 'Description : ';
-    const descriptionText = document.createElement('div');
-    descriptionText.classList.add('description-text');
-    descriptionText.textContent = task.description; 
-    descriptionDiv.appendChild(label);
-    descriptionDiv.appendChild(descriptionText);
-
-    const dueDateDiv = document.createElement('div');
-    dueDateDiv.classList.add('task-due-date')
-    dueDateDiv.textContent = task.dueDate;
-
+    
     const priorityDiv = document.createElement('div');
     priorityDiv.classList.add('task-priority');
     const priorityColor = document.createElement('img');
@@ -35,14 +23,31 @@ export const TaskNode = (task) => {
     priorityDiv.appendChild(priorityColor);
     priorityDiv.appendChild(priorityType);
 
+    const dueDateDiv = document.createElement('div');
+    dueDateDiv.classList.add('task-due-date')
+    dueDateDiv.textContent = task.dueDate;
+
+    taskHeader.appendChild(titleDiv);
+    taskHeader.appendChild(priorityDiv);
+    taskHeader.appendChild(dueDateDiv);
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('task-description');
+    const label = document.createElement('label');
+    label.classList.add('description-label');
+    label.textContent = 'Description : ';
+    const descriptionText = document.createElement('div');
+    descriptionText.classList.add('description-text');
+    descriptionText.textContent = task.description; 
+    descriptionDiv.appendChild(label);
+    descriptionDiv.appendChild(descriptionText);
+
     const notesDiv = document.createElement('div');
     notesDiv.classList.add('task-notes');
     notesDiv.textContent = task.notes;
 
-    taskNode.appendChild(titleDiv);
+    taskNode.appendChild(taskHeader);
     taskNode.appendChild(descriptionDiv);
-    taskNode.appendChild(dueDateDiv);
-    taskNode.appendChild(priorityDiv);
     taskNode.appendChild(notesDiv);
     return taskNode;
 };
