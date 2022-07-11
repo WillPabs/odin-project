cc7a61a287d5333699dc7fc122a3df576b3583fd
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-11 14:04:32 -0400

    Added priority enum

---
+++ Javascript/todo-list/src/priority.js
@@ -0,0 +1,18 @@
+import high from '../src/images/red_circle';
+import medium from '../src/images/yellow_circle';
+import low from '../src/images/blue_circle';
+
+export const PRIORITY = Object.freeze([
+    {
+        type : 'HIGH',
+        img : high
+    },
+    {
+        type : 'MEDIUM',
+        img : medium
+    },
+    {
+        type : 'LOW',
+        img : low
+    }
+]);
