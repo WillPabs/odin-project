d281828118625f66284770ed34a838d151862e06
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-12 16:57:33 -0400

    Reorganized code, added listener to editNode on click, added editNode function and editableClasses const

--- Javascript/todo-list/src/components/Task.js
+++ Javascript/todo-list/src/components/Task.js
@@ -2,52 +2,94 @@ import { PRIORITY } from "../priority";
 
 export const TaskNode = (task) => {
     const taskNode = document.createElement('div');
-    taskNode.classList.add('task');
-
     const taskHeader = document.createElement('div');
-    taskHeader.classList.add('task-header');
-    
     const titleDiv = document.createElement('div');
-    titleDiv.classList.add('task-title');
-    titleDiv.textContent = task.title;
-    
     const priorityDiv = document.createElement('div');
-    priorityDiv.classList.add('task-priority');
     const priorityColor = document.createElement('img');
-    priorityColor.classList.add('priority-color');
     const level = PRIORITY.find(p => p.type.toLowerCase() === task.priority);
-    priorityColor.src = level.img;
     const priorityType = document.createElement('div');
+    const dueDateDiv = document.createElement('div');
+    const descriptionDiv = document.createElement('div');
+    const label = document.createElement('label');
+    const descriptionText = document.createElement('div');
+    const notesDiv = document.createElement('div');
+
+    taskNode.classList.add('task');
+    taskHeader.classList.add('task-header');
+    titleDiv.classList.add('task-title');
+    priorityDiv.classList.add('task-priority');
+    priorityColor.classList.add('priority-color');
     priorityType.classList.add('priority-text');
+    dueDateDiv.classList.add('task-due-date')
+    descriptionDiv.classList.add('task-description');
+    label.classList.add('description-label');
+    descriptionText.classList.add('description-text');
+    notesDiv.classList.add('task-notes');
+
+    titleDiv.textContent = task.title;
+    priorityColor.src = level.img;
     priorityType.textContent = task.priority.toUpperCase();
+    dueDateDiv.textContent = task.dueDate;
+    label.textContent = 'Description : ';
+    descriptionText.textContent = task.description; 
+    notesDiv.textContent = task.notes;
+
+    taskNode.addEventListener('click', e => {
+        const targeted = e.target;
+        const className = targeted.classList.value;
+        if (editableClasses.find(name => name[0] === className)) {
+            editNode(targeted, editableClasses, task);
+        }
+    });
+    
+    
     priorityDiv.appendChild(priorityColor);
     priorityDiv.appendChild(priorityType);
-
-    const dueDateDiv = document.createElement('div');
-    dueDateDiv.classList.add('task-due-date')
-    dueDateDiv.textContent = task.dueDate;
-
     taskHeader.appendChild(titleDiv);
     taskHeader.appendChild(priorityDiv);
     taskHeader.appendChild(dueDateDiv);
-
-    const descriptionDiv = document.createElement('div');
-    descriptionDiv.classList.add('task-description');
-    const label = document.createElement('label');
-    label.classList.add('description-label');
-    label.textContent = 'Description : ';
-    const descriptionText = document.createElement('div');
-    descriptionText.classList.add('description-text');
-    descriptionText.textContent = task.description; 
     descriptionDiv.appendChild(label);
     descriptionDiv.appendChild(descriptionText);
-
-    const notesDiv = document.createElement('div');
-    notesDiv.classList.add('task-notes');
-    notesDiv.textContent = task.notes;
-
     taskNode.appendChild(taskHeader);
     taskNode.appendChild(descriptionDiv);
     taskNode.appendChild(notesDiv);
+
     return taskNode;
 };
+
+const editNode = (targeted, editableClasses, obj) => {
+    targeted.style.display = 'none';
+    let text = targeted.innerHTML;
+    const className = targeted.classList.value;
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = text;
+    console.log(targeted.parentNode)
+    targeted.parentNode.insertBefore(input, targeted);
+    input.focus();
+    input.onblur = () => {
+        // removes input element
+        targeted.parentNode.removeChild(input);
+
+        // assign value of input to inner html of targeted element
+        targeted.innerHTML = input.value == '' ? "&nbsp;" : input.value;
+
+        // match className with targeted element's className
+        const found = editableClasses.find(name => name[0] === className);
+
+        // call set method on obj with input value as an arg
+        obj[found[1]](input.value);
+
+        // display targeted element
+        targeted.style.display = '';
+    }
+}
+
+const editableClasses = [
+    ['task-title','setTitle'],
+    ['priority-text', 'setPriority'],
+    ['task-due-date', 'setDueDate'],
+    ['description-text', 'setDescription'],
+    ['task-notes', 'setNotes']
+]
