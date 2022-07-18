51fe67f4ba3dd75d8a76821c5880a60e0875d455
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-18 13:32:59 -0400

    Updated saveUser() and added setUser()

--- Javascript/todo-list/src/storage/user.js
+++ Javascript/todo-list/src/storage/user.js
@@ -1,9 +1,19 @@
 export const saveUser = (user) => {
     const user_serialized = JSON.stringify(user);
-    localStorage.setItem(user.id, user_serialized);
+    if (!localStorage.getItem(user.id)) {
+        localStorage.setItem(user.id, user_serialized);
+    } else {
+        setUser(user_serialized);
+    }
+    
 };
 
 export const getUser = (id) => {
     const user_deserialized = JSON.parse(localStorage.getItem(id));
     return user_deserialized;
+}
+
+const setUser = (user) => {
+    const user_deserialized = getUser(user.id);
+    user_deserialized.setUser(user);
 }
