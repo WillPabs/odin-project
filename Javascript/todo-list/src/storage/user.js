5ff8933f51a934b7b6aa882c23ec5e9d48432c77
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-18 16:05:13 -0400

    Updated saveUser() to update user if it already exists

--- Javascript/todo-list/src/storage/user.js
+++ Javascript/todo-list/src/storage/user.js
@@ -1,22 +1,19 @@
-import { User } from "../user";
-
 export const saveUser = (user) => {
     console.log(`Saving user ${user}`)
     if (!localStorage.getItem(user.id)) {
         const user_serialized = JSON.stringify(user);
         localStorage.setItem(user.id, user_serialized);
     } else {
-        setUser(user);
-    }
-    
+        const user_deserialized = getUser(user.id);
+        user_deserialized.name = user.name;
+        user_deserialized.projects = user.projects;
+        user_deserialized.finishedProjects = user.finishedProjects;
+        user_deserialized.profilePicture = user.profilePicture;
+        localStorage.setItem(user.id, JSON.stringify(user_deserialized));
+    };   
 };
 
 export const getUser = (id) => {
     const user_deserialized = JSON.parse(localStorage.getItem(id));
     return user_deserialized;
-}
-
-const setUser = (user) => {
-    const user_deserialized = User(getUser(user.id));
-    user_deserialized.updateUser(user);
-}
+};
