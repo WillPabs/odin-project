03ef9f38a893c8c359cb8a7d8a027298e27144fe
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-21 17:06:28 -0400

    Updated logic

--- Javascript/weather-app/src/index.js
+++ Javascript/weather-app/src/index.js
@@ -2,21 +2,22 @@ import config from './config';
 
 const key = config.API_KEY;
 
-const searchValue = document.querySelector('#search').value;
-
 // REQUEST BY CITY NAME
-const cityName = searchValue;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
-
-const getCityData = async () => {
-  console.log('Searching');
-  const response = await fetch(url, { mode: 'cors' });
-  const json = await response.json();
+const getCityData = async (cityName) => {
   const outputDiv = document.querySelector('#search-output');
-  outputDiv.textContent = json.data;
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+    console.log('Searching');
+    const response = await fetch(url, { mode: 'cors' });
+    const json = await response.json();
+    outputDiv.textContent = JSON.stringify(json);
+  } catch (err) {
+    outputDiv.textContent = err;
+  }
 };
 
 const searchButton = document.querySelector('#search-button');
-searchButton.addEventListener('submit', () => {
-  getCityData();
+searchButton.addEventListener('click', () => {
+  const cityName = document.querySelector('#search').value;
+  getCityData(cityName);
 });
