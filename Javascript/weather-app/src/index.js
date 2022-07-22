8581bb5706d9cb684b3c5219094660066b152fff
Will <pabiterowilliam@yahoo.com>
2022-07-22 02:25:27 -0400

    Added todo

--- Javascript/weather-app/src/index.js
+++ Javascript/weather-app/src/index.js
@@ -1,23 +1,34 @@
 import config from './config';
+import City from './components/City';
 
 const key = config.API_KEY;
 
+// OPTIONAL PARAMETERS
+const mode = 'JSON';
+const units = 'imperial';
+
+// TODO
+// Switch to using geocoding api instead of current weather
+
 // REQUEST BY CITY NAME
+const outputDiv = document.querySelector('#search-output');
 const getCityData = async (cityName) => {
-  const outputDiv = document.querySelector('#search-output');
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&appid=${key}`;
     console.log('Searching');
     const response = await fetch(url, { mode: 'cors' });
     const json = await response.json();
-    outputDiv.textContent = JSON.stringify(json);
+
+    const cityComponent = City(json);
+    outputDiv.appendChild(cityComponent);
   } catch (err) {
-    outputDiv.textContent = err;
+    outputDiv.textContent = `City not found. Enter another city.`;
   }
 };
 
 const searchButton = document.querySelector('#search-button');
 searchButton.addEventListener('click', () => {
+  if (outputDiv.textContent.length > 0) outputDiv.textContent = '';
   const cityName = document.querySelector('#search').value;
   getCityData(cityName);
 });
