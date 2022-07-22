85e8b2a8d38c2b482f2241a1ab83daa4c713675b
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-22 14:31:03 -0400

    Abstracted api request functions to api file

---
+++ Javascript/weather-app/src/api.js
@@ -0,0 +1,41 @@
+import config from './config';
+
+const key = config.API_KEY;
+
+const getCityData = async (lat, lon, units = 'imperial') => {
+  try {
+    const fullURL = await `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=${units}`;
+    const response = await fetch(fullURL, { mode: 'cors' });
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.log('Unable to fetch cityData');
+    return err;
+  }
+};
+
+const getCoordsByName = async (cityName, stateCode = 0, countryCode = 0, limit = 0) => {
+  try {
+    const fullURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${key}`;
+    const response = await fetch(fullURL, { mode: 'cors' });
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.log('Unable to fetch coordinates');
+    return err;
+  }
+};
+
+const getWeatherData = async (cityName) => {
+  const coords = await getCoordsByName(cityName);
+  const lat = await coords[0].lat;
+  const lon = await coords[0].lon;
+  const weatherData = await getCityData(lat, lon);
+  return weatherData;
+};
+
+export {
+  getCityData,
+  getCoordsByName,
+  getWeatherData,
+};
--- Javascript/weather-app/src/index.js
+++ Javascript/weather-app/src/index.js
@@ -1,34 +1,17 @@
-import config from './config';
-import City from './components/City';
+import { getWeatherData } from './api';
 
-const key = config.API_KEY;
-
-// OPTIONAL PARAMETERS
-const mode = 'JSON';
-const units = 'imperial';
-
-// TODO
-// Switch to using geocoding api instead of current weather
-
-// REQUEST BY CITY NAME
 const outputDiv = document.querySelector('#search-output');
-const getCityData = async (cityName) => {
-  try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&appid=${key}`;
-    console.log('Searching');
-    const response = await fetch(url, { mode: 'cors' });
-    const json = await response.json();
-
-    const cityComponent = City(json);
-    outputDiv.appendChild(cityComponent);
-  } catch (err) {
-    outputDiv.textContent = `City not found. Enter another city.`;
-  }
-};
-
 const searchButton = document.querySelector('#search-button');
 searchButton.addEventListener('click', () => {
   if (outputDiv.textContent.length > 0) outputDiv.textContent = '';
-  const cityName = document.querySelector('#search').value;
-  getCityData(cityName);
+  try {
+    const name = document.querySelector('#search').value;
+    const data = getWeatherData(name);
+    data.then((weatherData) => {
+      outputDiv.textContent = JSON.stringify(weatherData);
+    });
+  } catch (err) {
+    outputDiv.textContent = 'City not found. Enter another city.';
+    console.log(err);
+  }
 });
