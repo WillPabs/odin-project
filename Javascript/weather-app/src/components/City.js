97b0ba399a5289ce57a9f0b87206a87f87bb7ec2
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-25 13:15:50 -0400

    Removed log

--- Javascript/weather-app/src/components/City.js
+++ Javascript/weather-app/src/components/City.js
@@ -1,24 +1,23 @@
 const City = (cityData) => {
-    const container = document.createElement('div');
-    container.classList.add('city-container');
+  const container = document.createElement('div');
+  container.classList.add('city-container');
 
-    const nameDiv = document.createElement('div');
-    nameDiv.classList.add('city-name');
+  const nameDiv = document.createElement('div');
+  nameDiv.classList.add('city-name');
 
-    const tempDiv = document.createElement('div');
-    tempDiv.classList.add('city-current-temp');
+  const tempDiv = document.createElement('div');
+  tempDiv.classList.add('city-current-temp');
 
-    const cityName = cityData.name;
-    const country = cityData.sys.country;
-    const currentTemp = cityData.main.temp;
+  const cityName = cityData.name;
+  const { country } = cityData.sys;
+  const currentTemp = cityData.main.temp;
 
-    nameDiv.textContent = `${cityName}, ${country}`;
-    tempDiv.textContent = currentTemp;
+  nameDiv.textContent = `${cityName}, ${country}`;
+  tempDiv.textContent = currentTemp;
 
-    console.log(cityData);
-    container.appendChild(nameDiv);
-    container.appendChild(tempDiv);
-    return container;
-}
+  container.appendChild(nameDiv);
+  container.appendChild(tempDiv);
+  return container;
+};
 
 export default City;
