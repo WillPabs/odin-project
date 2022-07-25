07a5117f0b5e6a36965edb2569091240d9690071
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-25 13:52:55 -0400

    Added components

--- Javascript/weather-app/src/index.js
+++ Javascript/weather-app/src/index.js
@@ -1,4 +1,5 @@
-import { getWeatherData } from './api';
+import { getWeatherData, get5DayForecastData } from './api';
+import City from './components/City';
 
 const outputDiv = document.querySelector('#search-output');
 const searchButton = document.querySelector('#search-button');
@@ -8,7 +9,11 @@ searchButton.addEventListener('click', () => {
     const name = document.querySelector('#search').value;
     const data = getWeatherData(name);
     data.then((weatherData) => {
-      outputDiv.textContent = JSON.stringify(weatherData);
+      console.log(weatherData);
+      get5DayForecastData(weatherData.coord.lat, weatherData.coord.lon).then((wData) => {
+        console.log(wData);
+      });
+      outputDiv.appendChild(City(weatherData));
     });
   } catch (err) {
     outputDiv.textContent = 'City not found. Enter another city.';
