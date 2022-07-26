b3c14c1ae3e1ad9b601bc0ac4b30197468db0e41
Will <pabiterowilliam@yahoo.com>
2022-07-26 01:01:26 -0400

    Removed City component

--- Javascript/weather-app/src/index.js
+++ Javascript/weather-app/src/index.js
@@ -1,6 +1,5 @@
 import { getWeatherData, get5DayForecastData } from './api';
 import CurrentWeather from './components/CurrentWeather';
-import City from './components/City';
 
 const outputDiv = document.querySelector('#search-output');
 const searchButton = document.querySelector('#search-button');
@@ -10,11 +9,9 @@ searchButton.addEventListener('click', () => {
     const name = document.querySelector('#search').value;
     const data = getWeatherData(name);
     data.then((weatherData) => {
-      console.log(weatherData);
       get5DayForecastData(weatherData.coord.lat, weatherData.coord.lon).then((wData) => {
-        document.body.appendChild(CurrentWeather(wData));
+        outputDiv.appendChild(CurrentWeather(wData));
       });
-      outputDiv.appendChild(City(weatherData));
     });
   } catch (err) {
     outputDiv.textContent = 'City not found. Enter another city.';
