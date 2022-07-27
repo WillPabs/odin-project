455e419050adccff62a37adbd3a9854ba65d48a9
Will Pabs <pabiterowilliam@yahoo.com>
2022-07-27 14:08:32 -0400

    Created function to get data from list

--- Javascript/weather-app/src/components/DailyForecast.js
+++ Javascript/weather-app/src/components/DailyForecast.js
@@ -1,3 +1,4 @@
+import { format, isToday } from 'date-fns';
 import { getWeatherIcon } from '../api';
 
 const DailyForecast = (data) => {
@@ -19,32 +20,33 @@ const createForecastList = (list) => {
   const container = document.createElement('div');
   container.classList.add('forecast-list');
 
-  list.forEach((item) => {
+  const data = getDailyData(list);
+
+  for (let i = 0; i < 5; i += 1) {
     const day = document.createElement('div');
     day.classList.add('forecast-item');
 
     const date = document.createElement('div');
     date.classList.add('forecast-date');
-    date.textContent = item.dt_txt;
+    date.textContent = formatDate(data.dates[i]);
 
     const highTemp = document.createElement('div');
     highTemp.classList.add('forecast-high-temp');
-    highTemp.textContent = item.main.temp_max;
+    highTemp.textContent = `${data.highTemps[i]}°`;
 
     const lowTemp = document.createElement('div');
     lowTemp.classList.add('forecast-low-temp');
-    lowTemp.textContent = item.main.temp_min;
+    lowTemp.textContent = `${data.lowTemps[i]}°`;
 
     const icon = document.createElement('img');
     icon.classList.add('forecast-icon');
-    getWeatherIcon(item.weather[0].icon).then((data) => {
-      console.log(data);
-      icon.src = data;
+    getWeatherIcon(data.icons[i]).then((response) => {
+      icon.src = response;
     });
 
     const chanceRain = document.createElement('div');
     chanceRain.classList.add('forecast-chance-rain');
-    chanceRain.textContent = `${item.pop}%`;
+    chanceRain.textContent = `${data.rainChances[i]}%`;
 
     day.appendChild(date);
     day.appendChild(highTemp);
@@ -52,8 +54,43 @@ const createForecastList = (list) => {
     day.appendChild(icon);
     day.appendChild(chanceRain);
     container.appendChild(day);
-  });
+  }
   return container;
+};
+
+const formatDate = (date) => {
+  const newDate = new Date(date);
+  return isToday(newDate) ? 'Today' : format(newDate, 'E dd');
+};
+
+const getDailyData = (list) => {
+  const dates = [];
+  const highTemps = [];
+  const lowTemps = [];
+  const icons = [];
+  const rainChances = [];
+
+  let max = list[0].main.temp_max;
+  let min = list[0].main.temp_min;
+  let currentDate = list[0].dt_txt.split(' ')[0];
+  list.forEach((item) => {
+    if (item.dt_txt.split(' ')[0] === currentDate) {
+      if (max < item.main.temp_max) max = item.main.temp_max;
+      if (min > item.main.temp_min) min = item.main.temp_min;
+    } else {
+      dates.push(item.dt_txt.split(' ')[0]);
+      highTemps.push(max.toFixed(0));
+      lowTemps.push(min.toFixed(0));
+      icons.push(item.weather[0].icon);
+      rainChances.push(item.pop);
+      currentDate = item.dt_txt.split(' ')[0];
+      max = item.main.temp_max;
+      min = item.main.temp_min;
+    }
+  });
+  return {
+    dates, highTemps, lowTemps, icons, rainChances,
+  };
 };
 
 export default DailyForecast;
