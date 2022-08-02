3dcbcf8a33708fd1618f68f790eac911a917d5b2
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-02 13:41:44 -0400

    Added tests for receiveAttack function and abstracted logic into calculateShipPosition function

--- Javascript/battleship/src/gameboard.js
+++ Javascript/battleship/src/gameboard.js
@@ -1,24 +1,60 @@
 const Gameboard = (factor) => {
     let obj = Object.create(gameboardFunctions);
     obj.size = Array.from(Array(factor), () => (Array(factor)));
-    obj.attacks = [];
+    obj.missedAttacks = [];
     return obj;
 }
 
 const gameboardFunctions = {
-    placeShip(ship, coords) {
-        this.size[coords.x][coords.y] = ship;
+    placeShip(ship, coords, direction = 'horizontal') {
+        if (direction === 'vertical' && coords.x + ship.length > this.size.length) {
+            throw Error('Ship out of bounds');
+        }
+        if (direction === 'horizontal' && coords.y + ship.length > this.size[0].length) {
+            throw Error('Ship out of bounds');
+        }
+        for (let i = 0; i < ship.length; i++) {
+            direction === 'horizontal' ? 
+                this.size[coords.x][coords.y + i] = ship :
+                this.size[coords.x + i][coords.y] = ship;
+        }
     },
     receiveAttack(coords) {
-        const position = this.size[coords.x][coords.y];
-        if (position) {
-            position.hit(0);
+        let ship = this.size[coords.x][coords.y];
+        if (ship) {
+            const position = this.calculateShipPosition(coords);
+            ship.hit(position);
         } else {
-            this.attacks = coords;
+            this.missedAttacks.push(coords);
         }
     },
     allShipsSunk() {
 
+    },
+    calculateShipPosition(coords) {
+        let x = coords.x;
+        let y = coords.y;
+        let start = { x: x, y: y };
+        let position = 0;
+        if (x - 1 >= 0 && x + 1 < this.size.length) {
+            if (this.size[x - 1][y] !== undefined) {
+                while(x >= 0 && this.size[x][y] !== undefined) {
+                    start.x = x;
+                    x--;
+                }
+                if (coords.x !== start.x) position = coords.x - start.x;
+            }
+        }
+        if (y - 1 >= 0 && y + 1 < this.size[0].length) {
+            if (this.size[x][y - 1] !== undefined) {
+                while(y >= 0 && this.size[x][y] !== undefined) {
+                    start.y = y;
+                    y--;
+                }
+                if (coords.y !== start.y) position = coords.y - start.y;
+            }
+        }
+        return position;
     }
 }
 
--- Javascript/battleship/test/gameboard.test.js
+++ Javascript/battleship/test/gameboard.test.js
@@ -5,18 +5,18 @@ let board;
 let ship;
 
 beforeEach(() => {
-    board = Gameboard(4);
-    ship = Ship(1);
+    board = Gameboard(10);
+    ship = Ship(4);
 })
 
 test('Gameboard size is constructor param squared', () => {
     const size = board.size.length * board.size[0].length;
-    expect(size).toBe(16);
+    expect(size).toBe(100);
 });
 
 test('Attacks is initialized and empty', () => {
-    expect(board.attacks).toBeDefined();
-    expect(board.attacks).toEqual([]);
+    expect(board.missedAttacks).toBeDefined();
+    expect(board.missedAttacks).toEqual([]);
 })
 
 test('placeShip takes coords and places ship length on board', () => {
@@ -24,11 +24,26 @@ test('placeShip takes coords and places ship length on board', () => {
     expect(board.size[0][0]).toBe(ship);
 })
 
-test('receiveAttack function will miss', () => {
+test('receiveAttack function will miss. Coords on board will stay undefined and missedAttack will be pushed the coords', () => {
     board.receiveAttack({ x: 0, y: 0});
     expect(board.size[0][0]).toBeUndefined();
+    expect(board.missedAttacks[0]).toEqual({ x: 0, y: 0 });
+});
 
-    board.placeShip(ship, { x: 0, y: 0 });
-    board.receiveAttack({ x: 0, y: 0});
-    expect(board.size[0][0].hitArray[0]).toBeTruthy();
-})
+test('receiveAttack function will hit ship', () => {
+    board.placeShip(ship, { x: 1, y: 1 }, 'vertical');
+    board.receiveAttack({ x: 1, y: 1 });
+    expect(board.size[1][1].hitArray[0]).toBeTruthy();
+});
+
+test('receiveAttack function will hit a horizontal ship', () => {
+    board.placeShip(ship, { x: 3, y: 5 });
+    board.receiveAttack({ x: 3, y: 7 });
+    expect(board.size[3][7].hitArray[2]).toBeTruthy();
+});
+
+test('receiveAttack function will hit a vertical ship', () => {
+    board.placeShip(ship, { x: 2, y: 2 }, 'vertical');
+    board.receiveAttack({ x: 3, y: 2 });
+    expect(board.size[3][2].hitArray[1]).toBeTruthy();
+});
