0c3619b451dd51710644e24e9be5c1837bd757c7
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-04 14:38:53 -0400

    Added data

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -2,10 +2,19 @@ import GameboardComponent from './components/Gameboard';
 import CreatePlayer from './components/CreatePlayer';
 import Gameboard from './gameboard';
 import Player from './player';
+import './styles/style.css';
 
 const body = document.querySelector('body');
 
-const gameboard = Gameboard(8);
+const playerInput = CreatePlayer();
 
-const boardComponent = GameboardComponent(gameboard);
-body.appendChild(boardComponent);
+body.appendChild(playerInput);
+
+const gameboard1 = Gameboard(8);
+const gameboard2 = Gameboard(8);
+
+const boardComponent1 = GameboardComponent(gameboard1);
+const boardComponent2 = GameboardComponent(gameboard2);
+body.appendChild(boardComponent1);
+body.appendChild(document.createElement('br'));
+body.appendChild(boardComponent2);
