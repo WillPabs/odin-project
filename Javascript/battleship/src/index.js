831ea265de50fb65869458571de4cc74ce089c71
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-04 16:27:29 -0400

    Added classes for respective board

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -3,6 +3,7 @@ import CreatePlayer from './components/CreatePlayer';
 import Gameboard from './gameboard';
 import Player from './player';
 import './styles/style.css';
+import Game from './game';
 
 const body = document.querySelector('body');
 
@@ -14,7 +15,14 @@ const gameboard1 = Gameboard(8);
 const gameboard2 = Gameboard(8);
 
 const boardComponent1 = GameboardComponent(gameboard1);
+boardComponent1.classList.add('self');
 const boardComponent2 = GameboardComponent(gameboard2);
+boardComponent2.classList.add('rival');
 body.appendChild(boardComponent1);
 body.appendChild(document.createElement('br'));
 body.appendChild(boardComponent2);
+
+Game(
+  { board1: gameboard1, board2: gameboard2 },
+  { player1: Player('will'), player2: Player('bot') },
+);
