530682c7c6dda92726723ed5c95f564c594a6017
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-04 12:23:01 -0400

    Added CreatePlayer component

---
+++ Javascript/battleship/src/components/CreatePlayer.js
@@ -0,0 +1,19 @@
+const CreatePlayer = () => {
+    const container = document.createElement('div');
+    container.classList.add('create-player-container');
+
+    const label = document.createElement('label');
+    label.classList.add('create-player-label');
+    label.textContent = 'Enter Player Name';
+
+    const input = document.createElement('input');
+    input.classList.add('player-name-input');
+    input.type = 'text';
+    
+    container.appendChild(label);
+    container.appendChild(input);
+
+    return container;
+}
+
+export default CreatePlayer;
