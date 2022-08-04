5d4adadaa693f49b3b1af4652cd7861bee886349
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-04 16:26:43 -0400

    Added click events to respective boards

--- Javascript/battleship/src/game.js
+++ Javascript/battleship/src/game.js
@@ -1,9 +1,33 @@
-const Game = (players, boards) => {
+const Game = (boards, players) => {
   const { player1, player2 } = players;
   const { board1, board2 } = boards;
 
-  board1.addEventListener('click', (e) => {
-    console.log(e);
+  const board1El = document.querySelectorAll('.self tbody .cell');
+  const board2El = document.querySelector('.rival tbody .cell');
+
+  const playButton = document.querySelector('#play');
+  playButton.addEventListener('click', () => {
+
+  });
+
+  board1El.forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      const content = e.target.children[0];
+      const { x } = content.dataset;
+      const { y } = content.dataset;
+      player2.attack(x, y, board1);
+      console.log(board1.missedAttacks);
+    });
+  });
+
+  board2El.forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      const content = e.target.children[0];
+      const { x } = content.dataset;
+      const { y } = content.dataset;
+      player1.attack(x, y, board2);
+      console.log(board2.missedAttacks);
+    });
   });
 };
 
