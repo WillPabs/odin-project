39dfd6151b3e36e491ac7dda107160deb84edea9
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-05 13:52:58 -0400

    Added logic

--- Javascript/battleship/src/game.js
+++ Javascript/battleship/src/game.js
@@ -1,17 +1,27 @@
+import AttackList from './components/AttackList';
+import Coords from './coords';
+import Ship from './ship';
+
 const Game = (boards, players) => {
   const { player1, player2 } = players;
   const { board1, board2 } = boards;
 
+  const attackList1 = AttackList(board1);
+  const attackList2 = AttackList(board2);
+
   const board1El = document.querySelectorAll('.self tbody .cell');
-  const board2El = document.querySelector('.rival tbody .cell');
+  document.querySelector('.self').appendChild(attackList1);
+  const board2El = document.querySelectorAll('.rival tbody .cell');
+  document.querySelector('.rival').appendChild(attackList2);
 
   const playButton = document.querySelector('#play');
   playButton.addEventListener('click', () => {
-
+    console.log('Game Start');
   });
 
   board1El.forEach((cell) => {
     cell.addEventListener('click', (e) => {
+      console.log(e.target.dataset);
       const content = e.target.children[0];
       const { x } = content.dataset;
       const { y } = content.dataset;
