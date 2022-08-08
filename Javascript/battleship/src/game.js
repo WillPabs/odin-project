79d12f6b93462ee0559312d375cd1dfe7558d953
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-08 16:13:55 -0400

    Abstracted logic into one listener and added setTurn function

--- Javascript/battleship/src/game.js
+++ Javascript/battleship/src/game.js
@@ -17,30 +17,53 @@ const Game = (boards, players) => {
   const playButton = document.querySelector('#play');
   playButton.addEventListener('click', () => {
     console.log('Game Start');
+    play();
   });
 
-  board1El.forEach((cell) => {
-    cell.addEventListener('click', (e) => {
-      console.log(e.target);
-      const content = e.target.children[0];
-      const { x } = content.dataset;
-      const { y } = content.dataset;
-      player2.attack(x, y, board1);
+  const turnTracker = [];
+  const setTurn = () => {
+    if (turnTracker.length === 0) {
+      turnTracker.push(player1);
+      return player1;
+    }
 
-      board1.size[x][y] === undefined ? e.target.className = 'cell-miss' : e.target.className = 'cell-hit';
-      console.log(board1.missedAttacks);
+    // player2's turn
+    if (turnTracker[turnTracker.length - 1] === player1) {
+      turnTracker.push(player2);
+      return player2;
+    }
+
+    // player1's turn
+    if (turnTracker[turnTracker.length - 1] === player2) {
+      turnTracker.push(player1);
+      return player1;
+    }
+  };
+
+  let currentPlayer = setTurn();
+  const selfBoard = document.querySelector('.self');
+  const rivalBoard = document.querySelector('.rival');
+  const playing = () => {
+    document.querySelectorAll('.cell').forEach((cell) => {
+      cell.addEventListener('click', (e) => {
+        const content = e.target.children[0];
+        const { x } = content.dataset;
+        const { y } = content.dataset;
+        const boardElement = e.target.parentNode.parentNode.parentNode.parentNode;
+        const board = boardElement.classList.contains('self') ? board1 : board2;
+        const otherBoardElement = board === board1 ? rivalBoard : selfBoard;
+        console.log(otherBoardElement);
+        otherBoardElement.classList.remove('wait');
+        board.size[x][y] === undefined ? e.target.classList.add('cell-miss') : e.target.classList.add('cell-hit');
+        currentPlayer.attack(x, y, board);
+        boardElement.classList.add('wait');
+        if (board.allShipsSunk()) setTimeout(() => { alert('Game over'); }, 500);
+        currentPlayer = setTurn();
+      });
     });
-  });
+  };
 
-  board2El.forEach((cell) => {
-    cell.addEventListener('click', (e) => {
-      const content = e.target.children[0];
-      const { x } = content.dataset;
-      const { y } = content.dataset;
-      player1.attack(x, y, board2);
-      console.log(board2.missedAttacks);
-    });
-  });
+  playing();
 };
 
 export default Game;
