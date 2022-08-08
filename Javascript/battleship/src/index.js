44e7cfde7478ea0c03577690999e1205b5e7a0b3
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-08 14:08:23 -0400

    Updated to select from html layout

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -10,10 +10,7 @@ const body = document.querySelector('body');
 
 const playerInput = CreatePlayer();
 
-const playButton = document.createElement('button');
-playButton.id = 'play';
-playButton.textContent = 'Play';
-body.appendChild(playButton);
+const playButton = document.querySelector('#button');
 
 body.appendChild(playerInput);
 
@@ -21,13 +18,14 @@ const gameboard1 = Gameboard(10);
 const gameboard2 = Gameboard(10);
 gameboard1.generateShips();
 
+const field = document.querySelector('.battlefield');
 const boardComponent1 = GameboardComponent(gameboard1);
 boardComponent1.classList.add('self');
 const boardComponent2 = GameboardComponent(gameboard2);
 boardComponent2.classList.add('rival');
-body.appendChild(boardComponent1);
-body.appendChild(document.createElement('br'));
-body.appendChild(boardComponent2);
+field.appendChild(boardComponent1);
+field.appendChild(document.createElement('br'));
+field.appendChild(boardComponent2);
 
 Game(
   { board1: gameboard1, board2: gameboard2 },
