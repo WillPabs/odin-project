cdb4e888b7ba794737844bc5af07e3d5721953f3
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-09 14:08:33 -0400

    Refactor

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -5,6 +5,7 @@ import Player from './player';
 import Game from './game';
 import AttackList from './components/AttackList';
 import './styles/style.css';
+import Battlefield from './components/Battlefield';
 
 const body = document.querySelector('body');
 
@@ -19,16 +20,23 @@ const gameboard2 = Gameboard(10);
 gameboard1.generateShips();
 gameboard2.generateShips();
 
+const attackList1 = AttackList(gameboard1);
+const attackList2 = AttackList(gameboard2);
+
 const field = document.querySelector('.battlefields');
+
 const boardComponent1 = GameboardComponent(gameboard1);
 boardComponent1.element.classList.add('self');
 const boardComponent2 = GameboardComponent(gameboard2);
 boardComponent2.element.classList.add('rival');
-field.appendChild(boardComponent1.element);
-field.appendChild(document.createElement('br'));
-field.appendChild(boardComponent2.element);
+
+const field1 = Battlefield(boardComponent1, attackList1);
+const field2 = Battlefield(boardComponent2, attackList2);
+
+field.appendChild(field1.element);
+field.appendChild(field2.element);
 
 Game(
-  { board1: gameboard1, board2: gameboard2 },
+  { field1, field2 },
   { player1: Player('will'), player2: Player('bot') },
 );
