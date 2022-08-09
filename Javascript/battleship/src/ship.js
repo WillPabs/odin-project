05720999fe92d53c0e79705fa138603ef58d09ac
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-09 13:09:05 -0400

    Refactor

--- Javascript/battleship/src/ship.js
+++ Javascript/battleship/src/ship.js
@@ -1,17 +1,17 @@
 const Ship = (length) => {
-    let obj = Object.create(shipFunctions);
-    obj.length = length;
-    obj.hitArray = Array(length).fill(false);
-    return obj;
-}
+  const obj = Object.create(shipFunctions);
+  obj.length = length;
+  obj.hitArray = Array(length).fill(false);
+  return obj;
+};
 
 const shipFunctions = {
-    isSunk() {
-        return this.hitArray.every(hitbox => hitbox === true);
-    },
-    hit(num) {
-        this.hitArray[num] = true;
-    }
-}
+  isSunk() {
+    return this.hitArray.every((hitbox) => hitbox === true);
+  },
+  hit(num) {
+    this.hitArray[num] = true;
+  },
+};
 
 export default Ship;
