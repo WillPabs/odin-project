a1517e385f8affb9c424ed859b5479d7e11c84b6
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-10 14:11:14 -0400

    Refactor '

--- Javascript/battleship/src/components/Battlefield.js
+++ Javascript/battleship/src/components/Battlefield.js
@@ -1,9 +1,13 @@
+import ShipComponent from './Ship';
+import Ship from '../ship';
+
 const Battlefield = (board, attackList) => {
   const element = document.createElement('div');
   element.classList.add('battlefield');
 
   element.appendChild(board.element);
   element.appendChild(attackList.element);
+  element.appendChild(ShipComponent(Ship(2)));
 
   return {
     element,
