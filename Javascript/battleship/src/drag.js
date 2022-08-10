3626079db4d896d267af0f3543a8cc1560ff5bee
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-10 14:53:26 -0400

    Updated drop to be functional and add cell-busy class to every cell after according to length

--- Javascript/battleship/src/drag.js
+++ Javascript/battleship/src/drag.js
@@ -10,12 +10,19 @@ const Drag = (() => {
     ev.dataTransfer.setData('text', ev.target.id);
   };
 
+  /* TODO : allow entire ship box to be draggable */
   const drop = (ev) => {
     ev.preventDefault();
     if (ev.target.classList.contains('cell')) {
       console.log(ev.target);
       console.log(dragged);
-      ev.target.appendChild(dragged);
+      let { target } = ev;
+      target.appendChild(dragged);
+      const shipLength = dragged.dataset.length;
+      for (let i = 0; i < shipLength; i += 1) {
+        target.classList.add('cell-busy');
+        target = target.nextSibling;
+      }
     }
   };
 
