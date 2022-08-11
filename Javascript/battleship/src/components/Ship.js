1e607a8884f29ec72d76572939679d6c54bb8731
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-11 12:49:48 -0400

    Added id

--- Javascript/battleship/src/components/Ship.js
+++ Javascript/battleship/src/components/Ship.js
@@ -7,6 +7,8 @@ const ShipComponent = (ship) => {
 
   element.style.width = `${ship.length * 2}em`;
 
+  element.dataset.id = Math.floor(Math.random() * 1000);
+
   element.dataset.length = ship.length;
 
   return element;
