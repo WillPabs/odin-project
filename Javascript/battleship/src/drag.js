c88930cc5dca2d72f008d7fb1b82eef399dd391f
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-12 13:05:06 -0400

    Added logic to dragstart function to remove cell-busy class

--- Javascript/battleship/src/drag.js
+++ Javascript/battleship/src/drag.js
@@ -3,11 +3,20 @@ const Drag = (board) => {
 
   const start = (ev) => {
     dragged = ev.target;
+    let shipLength = dragged.dataset.length;
+    let newTarget = ev.target.parentNode.parentNode;
+    if (dragged.parentNode.parentNode.classList.contains('cell-busy')) {
+      while (shipLength > 0) {
+        newTarget.classList.remove('cell-busy');
+        newTarget = newTarget.nextSibling;
+        shipLength -= 1;
+      }
+    }
+    ev.dataTransfer.setData('text', ev.target.dataset.id);
   };
 
   const dragover = (ev) => {
     ev.preventDefault();
-    ev.dataTransfer.setData('text', ev.target.dataset.id);
   };
 
   const drop = (ev) => {
@@ -16,12 +25,11 @@ const Drag = (board) => {
     let newTarget = ev.target;
     while (shipLength > 0) {
       if (!newTarget) return;
+      if (newTarget.classList.contains('cell-busy')) return;
       newTarget = newTarget.nextSibling;
       shipLength -= 1;
     }
     if (ev.target.classList.contains('cell')) {
-      console.log(ev.target);
-      console.log(dragged);
       const { target } = ev;
       board.placeShip(target, dragged);
     }
