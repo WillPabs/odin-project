8b98909ae4bbf78d20470e80d3eba8b202724068
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-12 14:40:36 -0400

    Added function to check if cells within a 1 cell radius of ship is busy

--- Javascript/battleship/src/drag.js
+++ Javascript/battleship/src/drag.js
@@ -17,6 +17,9 @@ const Drag = (board) => {
 
   const dragover = (ev) => {
     ev.preventDefault();
+    // don't let ship drop if busy is true
+    const busy = checkIfBusy(ev.target, dragged.dataset.length);
+    if (busy) return;
   };
 
   const drop = (ev) => {
@@ -33,6 +36,48 @@ const Drag = (board) => {
       const { target } = ev;
       board.placeShip(target, dragged);
     }
+  };
+
+  const checkIfBusy = (target, length) => {
+    console.log(target);
+    const x = Number(target.firstChild.dataset.x);
+    const y = Number(target.firstChild.dataset.y);
+    for (let i = 0; i < length; i += 1) {
+      const left = document.querySelector(`.cell-content[data-x='${x}'][data-y='${y + i - 1}']`);
+      const right = document.querySelector(`.cell-content[data-x='${x}'][data-y='${y + i + 1}']`);
+      const up = document.querySelector(`.cell-content[data-x='${x + 1}'][data-y='${y + i}']`);
+      const down = document.querySelector(`.cell-content[data-x='${x - 1}'][data-y='${y + i}']`);
+      const lud = document.querySelector(`.cell-content[data-x='${x + 1}'][data-y='${y + i - 1}']`);
+      const lld = document.querySelector(`.cell-content[data-x='${x - 1}'][data-y='${y + i - 1}']`);
+      const rud = document.querySelector(`.cell-content[data-x='${x + 1}'][data-y='${y + i + 1}']`);
+      const rld = document.querySelector(`.cell-content[data-x='${x - 1}'][data-y='${y + i + 1}']`);
+      if (i === 0) {
+        console.log(up);
+        console.log(left);
+        console.log(down);
+        console.log(lud);
+        console.log(lld);
+        if (up.classList.contains('cell-busy')
+            || left.classList.contains('cell-busy')
+            || down.classList.contains('cell-busy')
+            || lud.classList.contains('cell-busy')
+            || lld.classList.contains('cell-busy')) { return true; }
+      } else if (i === length - 1) {
+        if (up.classList.contains('cell-busy')
+        || right.classList.contains('cell-busy')
+        || down.classList.contains('cell-busy')
+        || rud.classList.contains('cell-busy')
+        || rld.classList.contains('cell-busy')) { return true; }
+      } else if (up.classList.contains('cell-busy')
+        || down.classList.contains('cell-busy')) { return true; } else { return false; }
+    }
+    // loop through shiplength
+    // if beginning cell only check up, left, down, left upper and lower diagonal
+    // checks x+1; y-1; x-1; x+1, y-1; x-1, y-1;
+    // if end cell only check up, right, down, right upper and lower diagonal
+    // checks x+1; y+1; x-1; x+1, y+1; x-1, y+1;
+    // else check up, down
+    // checks x+1, x-1
   };
 
   return {
