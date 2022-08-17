c312f873385c2d1fb7d5a839c82e5f1d92b022f7
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-17 11:53:01 -0400

    Added a setLabel function

--- Javascript/battleship/src/components/Battlefield.js
+++ Javascript/battleship/src/components/Battlefield.js
@@ -4,15 +4,23 @@ const Battlefield = (board, attackList) => {
   const element = document.createElement('div');
   element.classList.add('battlefield');
 
-  const shipyard = Shipyard();
+  const label = document.createElement('div');
+  label.classList.add('battlefield-label');
+
+  element.appendChild(label);
   element.appendChild(board.element);
   element.appendChild(attackList.element);
-  element.appendChild(shipyard);
+  element.appendChild(Shipyard());
+
+  const setLabel = (text) => {
+    label.textContent = text;
+  };
 
   return {
     element,
     board,
     attackList,
+    setLabel,
   };
 };
 
