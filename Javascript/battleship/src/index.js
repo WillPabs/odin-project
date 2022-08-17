87f30078fb267d2701478f29e166b9395bd3cb3c
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-17 11:52:37 -0400

    Added labels to fields

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -11,8 +11,6 @@ const body = document.querySelector('body');
 
 const playerInput = CreatePlayer();
 
-const playButton = document.querySelector('#button');
-
 body.appendChild(playerInput);
 
 const gameboard1 = Gameboard(10);
@@ -25,12 +23,15 @@ const attackList2 = AttackList(gameboard2);
 const field = document.querySelector('.battlefields');
 
 const boardComponent1 = GameboardComponent(gameboard1);
-boardComponent1.element.classList.add('self');
 const boardComponent2 = GameboardComponent(gameboard2);
-boardComponent2.element.classList.add('rival');
 
 const field1 = Battlefield(boardComponent1, attackList1);
 const field2 = Battlefield(boardComponent2, attackList2);
+field1.element.classList.add('self');
+field2.element.classList.add('rival');
+
+field1.setLabel('Your Grid');
+field2.setLabel('Rival Grid');
 
 field.appendChild(field1.element);
 field.appendChild(field2.element);
