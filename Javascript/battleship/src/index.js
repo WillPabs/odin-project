68d95a18fd70ee5159bdbdc7415f450880e6c141
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-17 14:36:43 -0400

    Removed attackList variables

--- Javascript/battleship/src/index.js
+++ Javascript/battleship/src/index.js
@@ -17,16 +17,13 @@ const gameboard1 = Gameboard(10);
 const gameboard2 = Gameboard(10);
 gameboard2.generateShips();
 
-const attackList1 = AttackList(gameboard1);
-const attackList2 = AttackList(gameboard2);
-
 const field = document.querySelector('.battlefields');
 
 const boardComponent1 = GameboardComponent(gameboard1);
 const boardComponent2 = GameboardComponent(gameboard2);
 
-const field1 = Battlefield(boardComponent1, attackList1);
-const field2 = Battlefield(boardComponent2, attackList2);
+const field1 = Battlefield(boardComponent1);
+const field2 = Battlefield(boardComponent2);
 field1.element.classList.add('self');
 field2.element.classList.add('rival');
 
