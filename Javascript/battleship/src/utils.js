46459ba23c2e3172f65b57d55bce76116a78429b
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-30 14:18:19 -0400

    Add utils

---
+++ Javascript/battleship/src/utils.js
@@ -0,0 +1,16 @@
+import Battlefield from "./components/Battlefield";
+import GameboardComponent from "./components/Gameboard";
+import Gameboard from "./gameboard";
+
+export const reset = () => {
+    const gameboard1 = Gameboard(10);
+    const gameboard2 = Gameboard(10);
+    gameboard2.generateShips();
+
+    const boardComponent1 = GameboardComponent(gameboard1);
+    const boardComponent2 = GameboardComponent(gameboard2);
+
+    field1 = Battlefield(boardComponent1);
+    field2 = Battlefield(boardComponent2);
+    window.location.reload();
+};
