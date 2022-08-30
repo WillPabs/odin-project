f55cc5a7caba367f9ae27a2fed61fe9fb5ef6560
Will Pabs <pabiterowilliam@yahoo.com>
2022-08-30 13:14:27 -0400

    Fix calculateShipPosition

--- Javascript/battleship/src/gameboard.js
+++ Javascript/battleship/src/gameboard.js
@@ -38,23 +38,29 @@ const gameboardFunctions = {
     return this.ships.every((ship) => ship.isSunk());
   },
   calculateShipPosition(coords) {
-    let { x } = coords;
+    // let { x } = coords;
     let { y } = coords;
     const start = { x, y };
     let position = 0;
-    if (x - 1 >= 0 && x + 1 < this.size.length) {
-      if (this.size[x - 1][y] !== undefined) {
-        while (x >= 0 && this.size[x][y] !== undefined) {
-          start.x = x;
-          x--;
-        }
-        if (coords.x !== start.x) position = coords.x - start.x;
-      }
-    }
-    if (y - 1 >= 0 && y + 1 < this.size[0].length && x >= 0 && x < this.size.length) {
+    
+    // vertically placed ships
+    // if (x - 1 >= 0 && x + 1 < this.size.length) {
+    //   console.log(`x: ${x}`)
+    //   if (this.size[x - 1][y] !== undefined) {
+    //     while (x >= 0 && this.size[x][y] !== undefined) {
+    //       start.x = x;
+    //       x--;
+    //     }
+    //     if (coords.x !== start.x) position = coords.x - start.x;
+    //   }
+    // }
+
+    // horizontally placed ships
+    if (y - 1 >= 0 && y < this.size.length) {
       if (this.size[x][y - 1] !== undefined) {
         while (y >= 0 && this.size[x][y] !== undefined) {
           start.y = y;
+          console.log(`y: ${y}`)
           y--;
         }
         if (coords.y !== start.y) position = coords.y - start.y;
