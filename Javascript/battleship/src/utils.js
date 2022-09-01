5a7add511c8880a3e4f20c4db0f519bb45ae96f3
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-01 14:48:24 -0400

    Add checkAllShips function

--- Javascript/battleship/src/utils.js
+++ Javascript/battleship/src/utils.js
@@ -1,16 +1,29 @@
-import Battlefield from "./components/Battlefield";
-import GameboardComponent from "./components/Gameboard";
-import Gameboard from "./gameboard";
+import Battlefield from './components/Battlefield';
+import GameboardComponent from './components/Gameboard';
+import Gameboard from './gameboard';
 
-export const reset = (field1, field2) => {
-    const gameboard1 = Gameboard(10);
-    const gameboard2 = Gameboard(10);
-    gameboard2.generateShips();
+const reset = (field1, field2) => {
+  const gameboard1 = Gameboard(10);
+  const gameboard2 = Gameboard(10);
+  gameboard2.generateShips();
 
-    const boardComponent1 = GameboardComponent(gameboard1);
-    const boardComponent2 = GameboardComponent(gameboard2);
+  const boardComponent1 = GameboardComponent(gameboard1);
+  const boardComponent2 = GameboardComponent(gameboard2);
 
-    field1 = Battlefield(boardComponent1);
-    field2 = Battlefield(boardComponent2);
-    window.location.reload();
+  field1 = Battlefield(boardComponent1);
+  field2 = Battlefield(boardComponent2);
+  window.location.reload();
+};
+
+const checkAllShipsPlaced = () => {
+  const ships = document.querySelectorAll('.self .shipyard div');
+  for (const ship of ships) {
+    if (ship.children.length !== 0) return false;
+  }
+  return true;
+};
+
+export {
+  reset,
+  checkAllShipsPlaced,
 };
