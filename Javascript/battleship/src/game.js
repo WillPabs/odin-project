c09265e503a80fbf5da37d7a6e535b782a2c2e31
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-01 14:48:58 -0400

    Add listener

--- Javascript/battleship/src/game.js
+++ Javascript/battleship/src/game.js
@@ -1,6 +1,4 @@
-import Drag from './drag';
 import AttackList from './components/AttackList';
-import PlayAgain from './components/PlayAgain';
 import { reset } from './utils';
 
 const Game = (boards, players) => {
@@ -11,143 +9,112 @@ const Game = (boards, players) => {
   const setTurn = () => {
     if (turnTracker.length === 0) {
       turnTracker.push(player1);
-      return player1;
+      return { player: player1, field: field2 };
     }
 
     // player2's turn
     if (turnTracker[turnTracker.length - 1] === player1) {
       turnTracker.push(player2);
-      return player2;
+      return { player: player2, field: field1 };
     }
 
     // player1's turn
     if (turnTracker[turnTracker.length - 1] === player2) {
       turnTracker.push(player1);
-      return player1;
+      return { player: player1, field: field2 };
     }
   };
 
+  const makeMove = async (e, field, player) => {
+    field.board.receiveAttack(player, e.target);
+  };
+
+  const fullTurn = async (e, field, player) => {
+    field.board.element.classList.remove('wait');
+    await makeMove(e, field, player);
+    field.board.element.classList.add('wait');
+    field1.board.element.classList.remove('wait');
+    setTimeout(() => {
+      field1.board.receiveAttack(player2);
+      field1.board.element.classList.add('wait');
+      field2.board.element.classList.remove('wait');
+    }, 1000);
+  };
+
+  // game starts
+  document.querySelector('.self .shipyard').style.display = 'none';
+  const rival = document.querySelector('.rival');
+  const self = document.querySelector('.self .gameboard');
+  rival.classList.remove('wait');
+  self.classList.add('wait');
+  const attackList1 = AttackList(field1.board.gameboard);
+  const attackList2 = AttackList(field2.board.gameboard);
+  field1.setAttackList(attackList1);
+  field2.setAttackList(attackList2);
+  document.querySelectorAll('.ship-box').forEach((ship) => {
+    ship.parentNode.removeChild(ship);
+  });
+  document.querySelectorAll('.rival .cell').forEach((cell) => {
+    cell.addEventListener('click', (e) => fullTurn(e, field2, player1));
+  });
+
   // 1st turn will always be self
-  let currentPlayer = setTurn();
+  // let currentPlayer = setTurn();
   const selfBoard = document.querySelector('.self .gameboard');
   const rivalBoard = document.querySelector('.rival .gameboard');
 
-  // playing game with a bot
-  /* 
-    user clicks cell and attacks that board. The turn is passed to the bot where
-  */
+  // ({ currentPlayer, currentField } = setTurn());
+  // }
 
-  // playing game with another user
-  const makeMove = (e) => {
-    const content = e.target.children[0];
-    const { x } = content.dataset;
-    const { y } = content.dataset;
-    const boardElement = e.target.parentNode.parentNode.parentNode.parentNode;
-    const board = boardElement.parentNode.classList.contains('self') ? field1.board.gameboard : field2.board.gameboard;
-    const otherBoardElement = board === field1.board.gameboard ? rivalBoard : selfBoard;
-    const boardCells = boardElement.querySelectorAll('.cell');
-    const otherBoardCells = otherBoardElement.querySelectorAll('.cell');
-    if (!e.target.classList.contains('cell-miss') && !e.target.classList.contains('cell-hit')) {
-      board.size[x][y] === undefined ? e.target.classList.add('cell-miss') : e.target.classList.add('cell-hit');
-      currentPlayer.attack(x, y, board);
-      if (board.allShipsSunk()) {
-        // removeMove(boardCells);
-        removeMove(otherBoardCells);
-        setTimeout(() => {
-          console.log('Game Over');
-          document.querySelector('.battlefields').classList.add('wait');
-          const header = document.querySelector('header h2');
-          header.after(PlayAgain(currentPlayer.name));
-        }, 500);
-      }
-      if (e.target.classList.contains('cell-miss')) {
-        // removeMove(boardCells);
-        addMove(otherBoardCells);
-        boardElement.classList.add('wait');
-        otherBoardElement.classList.remove('wait');
-        currentPlayer = setTurn();
-        const otherBoard = boardElement.parentNode.classList.contains('self') ? field2.board.gameboard : field1.board.gameboard;
-        const botAttack = setInterval(() => {
-          const botAttackCoords = currentPlayer.botAttack(otherBoard);
-          const botX = botAttackCoords.x;
-          const botY = botAttackCoords.y;
-          const targetE = document.querySelector(`.cell-content[data-x='${botX}'][data-y='${botY}']`).parentNode;
-          otherBoard.size[botX][botY] === undefined ? targetE.classList.add('cell-miss') : targetE.classList.add('cell-hit');
-          console.log(`x: ${botX} y: ${botY} ${currentPlayer.name}`);
-          if (targetE.classList.contains('cell-miss')) {
-            // addMove(boardCells);
-            removeMove(otherBoardCells);
-            boardElement.classList.remove('wait');
-            otherBoardElement.classList.add('wait');
-            currentPlayer = setTurn();
-            console.log(`After turn ${currentPlayer.name}`);
-            clearInterval(botAttack);
-          }
-        }, 1000);
-      }
-    }
-  };
-
-  const removeMove = (board) => {
-    board.forEach((cell) => {
-      cell.removeEventListener('click', makeMove);
-    });
-  };
-
-  const addMove = (board) => {
-    board.forEach((cell) => {
-      cell.addEventListener('click', makeMove);
-    });
-  };
-
-  // make it specific to each board
-  const playing = () => {
-    document.querySelector('.self .shipyard').style.display = 'none';
-    const rival = document.querySelector('.rival');
-    const self = document.querySelector('.self .gameboard');
-    rival.classList.remove('wait');
-    self.classList.add('wait');
-    const attackList1 = AttackList(field1.board.gameboard);
-    const attackList2 = AttackList(field2.board.gameboard);
-    field1.setAttackList(attackList1);
-    field2.setAttackList(attackList2);
-    document.querySelectorAll('.ship-box').forEach((ship) => {
-      ship.parentNode.removeChild(ship);
-    });
-    document.querySelectorAll('.rival .cell').forEach((cell) => {
-      cell.addEventListener('click', makeMove);
-    });
-  };
-
-  const setUp = (board) => {
-    console.log('setting up');
-    document.querySelector('.rival').classList.add('wait');
-    const drag = Drag(board);
-    document.querySelectorAll('.ship-box').forEach((ship) => ship.addEventListener('dragstart', drag.start));
-    document.querySelectorAll('.self .cell').forEach((cell) => cell.addEventListener('dragover', drag.dragover));
-    document.querySelectorAll('.self .cell').forEach((cell) => cell.addEventListener('drop', drag.drop));
-  };
-
-  setUp(field1.board);
-
-  const playButton = document.querySelector('#play');
-  playButton.addEventListener('click', () => {
-    if (!checkAllShipsPlaced()) {
-      alert('Please place all remaining ships on the board');
-    } else {
-      console.log('Game Start');
-      playing();
-      playButton.style.display = 'none';
-    }
-  });
-
-  const checkAllShipsPlaced = () => {
-    const ships = document.querySelectorAll('.self .shipyard div');
-    for (const ship of ships) {
-      if (ship.children.length !== 0) return false;
-    }
-    return true;
-  };
+  // const makeMove = (e) => {
+  //   const content = e.target.children[0];
+  //   const { x } = content.dataset;
+  //   const { y } = content.dataset;
+  //   const boardElement = e.target.parentNode.parentNode.parentNode.parentNode;
+  //   const board = boardElement.parentNode.classList.contains('self') ? field1.board.gameboard : field2.board.gameboard;
+  //   const otherBoardElement = board === field1.board.gameboard ? rivalBoard : selfBoard;
+  //   const boardCells = boardElement.querySelectorAll('.cell');
+  //   const otherBoardCells = otherBoardElement.querySelectorAll('.cell');
+  //   if (!e.target.classList.contains('cell-miss') && !e.target.classList.contains('cell-hit')) {
+  //     board.size[x][y] === undefined ? e.target.classList.add('cell-miss') : e.target.classList.add('cell-hit');
+  //     currentPlayer.attack(x, y, board);
+  //     if (board.allShipsSunk()) {
+  //       // removeMove(boardCells);
+  //       removeMove(otherBoardCells);
+  //       setTimeout(() => {
+  //         console.log('Game Over');
+  //         document.querySelector('.battlefields').classList.add('wait');
+  //         const header = document.querySelector('header h2');
+  //         header.after(PlayAgain(currentPlayer.name));
+  //       }, 500);
+  //     }
+  //     if (e.target.classList.contains('cell-miss')) {
+  //       // removeMove(boardCells);
+  //       addMove(otherBoardCells);
+  //       boardElement.classList.add('wait');
+  //       otherBoardElement.classList.remove('wait');
+  //       currentPlayer = setTurn();
+  //       const otherBoard = boardElement.parentNode.classList.contains('self') ? field2.board.gameboard : field1.board.gameboard;
+  //       const botAttack = setInterval(() => {
+  //         const botAttackCoords = currentPlayer.botAttack(otherBoard);
+  //         const botX = botAttackCoords.x;
+  //         const botY = botAttackCoords.y;
+  //         const targetE = document.querySelector(`.cell-content[data-x='${botX}'][data-y='${botY}']`).parentNode;
+  //         otherBoard.size[botX][botY] === undefined ? targetE.classList.add('cell-miss') : targetE.classList.add('cell-hit');
+  //         console.log(`x: ${botX} y: ${botY} ${currentPlayer.name}`);
+  //         if (targetE.classList.contains('cell-miss')) {
+  //           // addMove(boardCells);
+  //           removeMove(otherBoardCells);
+  //           boardElement.classList.remove('wait');
+  //           otherBoardElement.classList.add('wait');
+  //           currentPlayer = setTurn();
+  //           console.log(`After turn ${currentPlayer.name}`);
+  //           clearInterval(botAttack);
+  //         }
+  //       }, 1000);
+  //     }
+  //   }
+  // };
 
   const resetButton = document.querySelector('#leave-game');
   resetButton.addEventListener('click', () => {
