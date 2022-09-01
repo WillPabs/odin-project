0149787e709d445189dd95594525d75cac15a7f2
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-01 14:47:55 -0400

    Add setup

---
+++ Javascript/battleship/src/setup.js
@@ -0,0 +1,12 @@
+import Drag from './drag';
+
+const setUp = async (board) => {
+  console.log('setting up');
+  document.querySelector('.rival').classList.add('wait');
+  const drag = Drag(board);
+  document.querySelectorAll('.ship-box').forEach((ship) => ship.addEventListener('dragstart', drag.start));
+  document.querySelectorAll('.self .cell').forEach((cell) => cell.addEventListener('dragover', drag.dragover));
+  document.querySelectorAll('.self .cell').forEach((cell) => cell.addEventListener('drop', drag.drop));
+};
+
+export default setUp;
