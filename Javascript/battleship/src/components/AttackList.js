81d72d4e4cc8c0b25385756739bddc902c8b590c
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-06 15:24:30 -0400

    Add update function

--- Javascript/battleship/src/components/AttackList.js
+++ Javascript/battleship/src/components/AttackList.js
@@ -6,11 +6,18 @@ const AttackList = (board) => {
   console.log(ships);
 
   ships.forEach((ship) => {
+    const { length } = ship;
     const shipHitArray = document.createElement('div');
     shipHitArray.classList.add('ship');
+    shipHitArray.dataset.id = ship.id;
+    shipHitArray.classList.add(`length-${length}`);
+
+    let index = 1;
     ship.hitArray.forEach(() => {
       const shipPart = document.createElement('div');
       shipPart.classList.add('ship-part');
+      shipPart.classList.add(`part-${index}`);
+      index += 1;
       shipHitArray.appendChild(shipPart);
     });
 
@@ -27,9 +34,16 @@ const AttackList = (board) => {
       });
     });
   };
+
+  const update = (id) => {
+    const ship = document.querySelector(`.ship[data-id='${id}']`);
+    const found = [...ship.children].find((part) => !part.classList.contains('hit'));
+    found.classList.add('hit');
+  };
   return {
     element,
     render,
+    update,
   };
 };
 
